47c7ff2968492ab446037a077bdc73481530b579
Lars Berger <lars.berger@yahoo.com>
2023-07-02 14:44:25 +0800

    feat(de): progress on creating sample config if it doesn't exist

--- apps/desktop/src/user_config.rs
+++ apps/desktop/src/user_config.rs
@@ -1,17 +1,34 @@
 use std::fs;
 
 use anyhow::{Context, Result};
-use tauri::api::path::home_dir;
+use tauri::{api::path::home_dir, AppHandle};
 
 /// Reads the config file at `/.glazer/zanbar.yaml` in the user's home
 /// directory.
-pub fn read_file() -> Result<String> {
-  let path = home_dir()
+pub fn read_file(app_handle: AppHandle) -> Result<String> {
+  let config_path = home_dir()
     .context("Unable to get home directory.")?
     .join(".glazer")
     .join("zanbar.yaml");
 
-  let config_string = fs::read(path)
+  let sample_path = app_handle
+    .path_resolver()
+    .resolve_resource("sample-config.yaml")
+    .expect("Failed to resolve resource.");
+
+  std::fs::create_dir_all(&config_path).with_context(|| {
+    format!("Failed to create directory {}.", &config_path.display())
+  })?;
+
+  fs::copy(&sample_path, &config_path).with_context(|| {
+    format!(
+      "Failed to copy file from {} to {}.",
+      &sample_path.display(),
+      &config_path.display()
+    )
+  })?;
+
+  let config_string = fs::read(&config_path)
     .context("Unable to read config file.")?
     .iter()
     .map(|&c| c as char)
