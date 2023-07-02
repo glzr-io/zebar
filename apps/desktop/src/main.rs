6bad5d6d5b410bc04956c74f20bfcef3cf97b354
Lars Berger <lars.berger@accedo.tv>
2023-07-02 14:44:25 +0800

    feat(de): progress on creating sample config if it doesn't exist

--- apps/desktop/src/main.rs
+++ apps/desktop/src/main.rs
@@ -1,11 +1,13 @@
 // Prevents additional console window on Windows in release.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::AppHandle;
+
 mod user_config;
 
 #[tauri::command]
-fn read_config_file() -> Result<String, String> {
-  user_config::read_file().map_err(|err| err.to_string())
+fn read_config_file(app_handle: AppHandle) -> Result<String, String> {
+  user_config::read_file(app_handle).map_err(|err| err.to_string())
 }
 
 fn main() {
