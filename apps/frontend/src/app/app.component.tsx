4e6e1a88cb108bb8b260e51349563ac32c701e20
Lars Berger <lars.berger@yahoo.com>
2023-07-02 12:07:34 +0800

    feat(fe): compile scss on the fly

--- apps/frontend/src/app/app.component.tsx
+++ apps/frontend/src/app/app.component.tsx
@@ -1,10 +1,19 @@
 import { Show } from 'solid-js';
+import { compileString } from 'sass';
 
 import s from './app.module.scss';
 import { useConfig } from './shared/use-config.hook';
 
 export function App() {
   const config = useConfig();
+
+  console.log(
+    compileString(`
+      .box {
+        width: 10px + 15px;
+      }
+    `),
+  );
 
   return (
     <Show when={config()}>
