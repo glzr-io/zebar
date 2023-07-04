8d2eaa7da2909345cf93a9b3276edaf31083750d
Lars Berger <lars.berger@yahoo.com>
2023-07-04 20:21:37 +0800

    feat(fe): escape bindings that shouldn't be compiled by nunjucks

--- apps/frontend/src/app/shared/utils/parse-template.ts
+++ apps/frontend/src/app/shared/utils/parse-template.ts
@@ -15,11 +15,51 @@ export function parseTemplate(
 ): HTMLElement {
   const { bindings = {} } = options;
 
-  // Nunjucks is used to evaluate strings in the template.
-  const compiledTemplate = renderString(template, bindings.strings ?? {});
+  const compiledTemplate = parseTemplateStrings(
+    template,
+    bindings.strings ?? {},
+    {
+      bindingsToEscape: [
+        ...Object.keys(bindings.functions ?? {}),
+        ...Object.keys(bindings.components ?? {}),
+      ],
+    },
+  );
 
   const element = document.createElement('div');
   element.innerHTML = compiledTemplate;
 
   return element;
+}
+
+export interface ParseTemplateStringsOptions {
+  bindingsToEscape?: string[];
+}
+
+/**
+ * Nunjucks is used to evaluate strings in the template.
+ */
+function parseTemplateStrings(
+  template: string,
+  bindings: Record<string, string | boolean | number>,
+  options: ParseTemplateStringsOptions = {},
+): string {
+  const { bindingsToEscape = [] } = options;
+
+  // Need to somehow ignore bindings that shouldn't be compiled by Nunjucks.
+  // Accomplish this by wrapping them in '{{ }}'.
+  const escapedBindings = bindingsToEscape.reduce(
+    (acc, binding) => ({
+      ...acc,
+      [binding]: `{{ ${binding} }}`,
+    }),
+    bindings,
+  );
+
+  const compiledTemplate = renderString(template, {
+    ...bindings,
+    ...escapedBindings,
+  });
+
+  return compiledTemplate;
 }
