0dcb0f16682868147072f9ab9ff1d853af148995
Lars Berger <lars.berger@accedo.tv>
2023-07-04 19:34:27 +0800

    feat(fe): pass `bindings` options object to `parseTemplate`

--- apps/frontend/src/app/shared/utils/parse-template.ts
+++ apps/frontend/src/app/shared/utils/parse-template.ts
@@ -1,10 +1,22 @@
 import { renderString } from 'nunjucks';
+import { JSXElement } from 'solid-js';
+
+export interface ParseTemplateOptions {
+  bindings?: {
+    strings?: Record<string, string | boolean | number>;
+    functions?: Record<string, (...args: unknown[]) => unknown>;
+    components?: Record<string, () => JSXElement>;
+  };
+}
 
 export function parseTemplate(
   template: string,
-  context: Record<string, unknown>,
+  options: ParseTemplateOptions,
 ): HTMLElement {
-  const compiledTemplate = renderString(template, context);
+  const { bindings = {} } = options;
+
+  // Nunjucks is used to evaluate strings in the template.
+  const compiledTemplate = renderString(template, bindings.strings ?? {});
 
   const element = document.createElement('div');
   element.innerHTML = compiledTemplate;
