41744b2dea56591872062df41281016086b11bc8
Lars Berger <lars.berger@yahoo.com>
2023-07-10 20:25:55 +0800

    feat(fe): consume `toRecordType` in user config types

--- apps/frontend/src/app/shared/user-config/types/user-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/user-config.model.ts
@@ -1,12 +1,13 @@
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 
 import { BarConfig } from './bar/bar-config.model';
 import { GeneralConfig } from './general-config.model';
+import { toRecordType } from '~/shared/utils';
 
 export class UserConfig {
   @Type(() => GeneralConfig)
   general: GeneralConfig;
 
-  @Type(() => BarConfig)
+  @Transform(toRecordType(BarConfig))
   bar: Record<string, BarConfig>;
 }
