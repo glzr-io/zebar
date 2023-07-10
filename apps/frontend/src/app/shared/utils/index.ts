9daa065b8fa9ce337b767609294bda01267dc313
Lars Berger <lars.berger@yahoo.com>
2023-07-11 03:05:33 +0800

    feat(fe): create `toCamelCase` util

--- apps/frontend/src/app/shared/utils/index.ts
+++ apps/frontend/src/app/shared/utils/index.ts
@@ -2,5 +2,6 @@ export * from './create-unique-id';
 export * from './insert-and-replace';
 export * from './is-object';
 export * from './memoize';
+export * from './to-camel-case';
 export * from './to-record-type';
 export * from './validate-record';
