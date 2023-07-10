dbada955b49348fe90b5afa0e86b5289844cbe11
Lars Berger <lars.berger@yahoo.com>
2023-07-11 05:39:04 +0800

    feat(fe): create `delimitedKey` for improving `bar/` and `group/` validation

--- apps/frontend/src/app/shared/user-config/types/user-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/user-config.model.ts
@@ -3,16 +3,13 @@ import { z } from 'zod';
 import { BarConfig } from './bar/bar-config.model';
 import { GeneralConfig } from './general-config.model';
 import { Prettify } from '~/shared/utils';
-
-const getKey = z.custom<`bar/${string}`>(val => {
-  return (val as string).startsWith('bar/');
-});
+import { delimitedKey } from '../delimited-key';
 
 export const UserConfig = z
   .object({
     general: GeneralConfig.optional(),
     bar: BarConfig.optional(),
   })
-  .and(z.record(getKey, BarConfig).optional());
+  .and(z.record(delimitedKey('bar'), BarConfig).optional());
 
 export type UserConfig = Prettify<z.infer<typeof UserConfig>>;
