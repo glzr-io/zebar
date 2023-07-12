3f207ed747b8ce83a1ec0ad055c73800ea33a350
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:46:20 +0800

    feat(fe): handle `bar/*` keys in user config schema via `.superRefine`

--- apps/frontend/src/app/shared/user-config/types/user-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/user-config.model.ts
@@ -1,15 +1,37 @@
 import { z } from 'zod';
 
-import { BarConfigSchema } from './bar/bar-config.model';
-import { delimitedKey } from '../delimited-key';
+import { BarConfig, BarConfigSchema } from './bar/bar-config.model';
 import { GeneralConfigSchema } from './general-config.model';
 import { Prettify } from '~/shared/utils';
 
-export const UserConfigSchema = z
-  .object({
-    general: GeneralConfigSchema,
-    bar: BarConfigSchema.optional(),
-  })
-  .and(z.record(delimitedKey('bar'), BarConfigSchema).optional());
+const UserConfigSchema1 = z.object({
+  general: GeneralConfigSchema,
+  bar: BarConfigSchema.optional(),
+});
+
+type UserConfig1 = z.infer<typeof UserConfigSchema1>;
+
+export const UserConfigSchema = UserConfigSchema1.passthrough().superRefine(
+  (arg, ctx): arg is UserConfig1 & { [key: `bar/${string}`]: BarConfig } => {
+    const barKeys = Object.keys(arg).filter(key => key.startsWith('bar/'));
+
+    for (const key of barKeys) {
+      const res = BarConfigSchema.safeParse(arg[key]);
+
+      if (res.success) {
+        continue;
+      }
+
+      for (const issue of res.error.issues) {
+        ctx.addIssue({
+          ...issue,
+          path: [key, ...issue.path],
+        });
+      }
+    }
+
+    return z.NEVER;
+  },
+);
 
 export type UserConfig = Prettify<z.infer<typeof UserConfigSchema>>;
