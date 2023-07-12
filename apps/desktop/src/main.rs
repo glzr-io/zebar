ae70f11e143235a60ce984361a58e8ae59e27484
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:40:16 +0800

    feat(de): add single instance plugin and basic CLI setup

--- apps/desktop/src/main.rs
+++ apps/desktop/src/main.rs
@@ -1,9 +1,15 @@
 // Prevents additional console window on Windows in release.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::AppHandle;
+use tauri::{AppHandle, Manager};
 
 mod user_config;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
 
 #[tauri::command]
 fn read_config_file(
@@ -16,6 +22,21 @@ fn read_config_file(
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      match app.get_cli_matches() {
+        Ok(matches) => {
+          println!("{:?}", matches);
+        }
+        Err(_) => panic! {"CLI Parsing Error"},
+      };
+      Ok(())
+    })
+    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+      println!("{}, {argv:?}, {cwd}", app.package_info().name);
+      app
+        .emit_all("single-instance", Payload { args: argv, cwd })
+        .unwrap();
+    }))
     .invoke_handler(tauri::generate_handler![read_config_file])
     .run(tauri::generate_context!())
     .expect("Error while running Tauri application.");
