a4310cffc14cbdef838f81350e3efe7e33ed37f1
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:31:00 +0800

    feat(fe): minor user config validation changes

--- apps/frontend/src/app/shared/user-config/types/shared/element.model.ts
+++ apps/frontend/src/app/shared/user-config/types/shared/element.model.ts
@@ -3,18 +3,21 @@ import { z } from 'zod';
 import { ScriptVariableConfigSchema } from './script-variable-config.model';
 import { createUniqueId } from '~/shared/utils';
 
-export const ElementSchema = z.object({
-  id: z.string().default(createUniqueId),
-  class_name: z.string(),
-  styles: z.string(),
-  template_variables: z.record(
-    z.string(),
-    z.union([z.string(), ScriptVariableConfigSchema]),
-  ),
-  template_commands: z.record(z.string(), z.string()),
-  template: z.string(),
-  label: z.string(),
-});
+export const ElementSchema = z
+  .object({
+    id: z.string().default(createUniqueId),
+    class_name: z.string(),
+    styles: z.string(),
+    template_variables: z.record(
+      z.string(),
+      z.union([z.string(), ScriptVariableConfigSchema]),
+    ),
+    template_commands: z.record(z.string(), z.string()),
+    template: z.string(),
+    label: z.string(),
+  })
+  .partial()
+  .required({ id: true });
 
 /** Base type for elements with a template. */
 export type Element = z.infer<typeof ElementSchema>;
--- apps/frontend/src/app/shared/user-config/types/shared/script-variable-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/shared/script-variable-config.model.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const ScriptVariableConfigSchema = z.object({
   source: z.literal('script'),
   script_path: z.string(),
-  script_args: z.string(),
-  refresh_interval_ms: z.number(),
+  script_args: z.string().optional(),
+  refresh_interval_ms: z.number().default(5000),
 });
 
 export type ScriptVariableConfig = z.infer<typeof ScriptVariableConfigSchema>;
