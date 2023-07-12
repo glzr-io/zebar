21bdd5752fafdbb702d4c4a899fa6ae966ac9cac
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:28:04 +0800

    feat(fe): improve strictness of `delimitedKey`

--- apps/frontend/src/app/shared/user-config/delimited-key.ts
+++ apps/frontend/src/app/shared/user-config/delimited-key.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export function delimitedKey<const T extends string>(prefix: T) {
-  return z.custom<`${T}/${string}`>(val => {
-    return (val as string).startsWith(`${prefix}/`);
-  });
+  return z.string().pipe(
+    z.custom<`${T}/${string}`>(val => {
+      return (val as string).startsWith(`${prefix}/`);
+    }),
+  );
 }
