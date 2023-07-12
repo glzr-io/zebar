09db180c5784c1337a3b6eec11bf297682addddf
Lars Berger <lars.berger@accedo.tv>
2023-07-13 03:44:52 +0800

    feat(fe): add default `class_name` to component configs

--- apps/frontend/src/app/shared/user-config/types/bar/components/clock-component-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/components/clock-component-config.model.ts
@@ -4,6 +4,7 @@ import { ElementSchema } from '../../shared/element.model';
 
 export const ClockComponentConfigSchema = ElementSchema.extend({
   type: z.literal('clock'),
+  class_name: z.string().default('clock-component'),
 });
 
 export type ClockComponentConfig = z.infer<typeof ClockComponentConfigSchema>;
