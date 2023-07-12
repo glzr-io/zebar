b8ee6b0f2931446a8aa17d9367cad3ee6a969db4
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:31:00 +0800

    feat(fe): minor user config validation changes

--- apps/frontend/src/app/shared/user-config/types/bar/component-group-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/component-group-config.model.ts
@@ -4,7 +4,7 @@ import { ComponentConfigSchema } from './component-config.model';
 import { ElementSchema } from '../shared/element.model';
 
 export const ComponentGroupConfigSchema = ElementSchema.extend({
-  components: z.array(ComponentConfigSchema),
+  components: z.array(ComponentConfigSchema).default([]),
 });
 
 export type ComponentGroupConfig = z.infer<typeof ComponentGroupConfigSchema>;
