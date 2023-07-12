87d3eddb4fc999f1c37476ec1f94b8a70ae5bbfe
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:44:52 +0800

    feat(fe): add default `class_name` to component configs

--- apps/frontend/src/app/shared/user-config/types/bar/components/cpu-component-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/components/cpu-component-config.model.ts
@@ -4,7 +4,8 @@ import { ElementSchema } from '../../shared/element.model';
 
 export const CpuComponentConfigSchema = ElementSchema.extend({
   type: z.literal('cpu'),
-  refresh_interval_ms: z.number(),
+  class_name: z.string().default('cpu-component'),
+  refresh_interval_ms: z.number().default(5000),
 });
 
 export type CpuComponentConfig = z.infer<typeof CpuComponentConfigSchema>;
--- apps/frontend/src/app/shared/user-config/types/bar/components/glazewm-component-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/components/glazewm-component-config.model.ts
@@ -4,6 +4,7 @@ import { ElementSchema } from '../../shared/element.model';
 
 export const GlazeWMComponentConfigSchema = ElementSchema.extend({
   type: z.literal('glazewm'),
+  class_name: z.string().default('glazewm-component'),
 });
 
 export type GlazeWMComponentConfig = z.infer<
