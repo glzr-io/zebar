298d68a8636cececc4b3a9e870895113be0f5bc5
Lars Berger <lars.berger@yahoo.com>
2023-07-13 03:28:46 +0800

    feat(fe): dynamically create `<style>` tag from built css

--- apps/frontend/src/app/app.component.tsx
+++ apps/frontend/src/app/app.component.tsx
@@ -1,15 +1,32 @@
-import { Show } from 'solid-js';
+import { Show, createEffect, on } from 'solid-js';
 import { configure } from 'nunjucks';
 
 import s from './app.module.scss';
 import { Bar } from './bar/bar';
-import { useUserConfig } from './shared/user-config';
+import { useStyleBuilder, useUserConfig } from './shared/user-config';
 
 export function App() {
   const userConfig = useUserConfig();
+  const styleBuilder = useStyleBuilder();
 
   // Prevent Nunjucks from escaping HTML.
   configure({ autoescape: false });
+
+  // Dynamically create <style> tag and append it to <head>.
+  createEffect(
+    on(
+      () => styleBuilder.builtCss(),
+      builtCss => {
+        if (builtCss) {
+          const styleElement = document.createElement('style');
+          document.head.appendChild(styleElement);
+          styleElement.innerHTML = builtCss;
+
+          return () => document.head.removeChild(styleElement);
+        }
+      },
+    ),
+  );
 
   return (
     <Show when={userConfig.barConfig()}>
