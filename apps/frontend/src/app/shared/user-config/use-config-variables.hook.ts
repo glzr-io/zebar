8d5b49407d15ec2170ab0815e156c55bf1914e42
Lars Berger <lars.berger@accedo.tv>
2023-07-14 01:58:08 +0800

    feat(fe): create `useConfigVariables` hook for getting variables to replace in the config

---
+++ apps/frontend/src/app/shared/user-config/use-config-variables.hook.ts
@@ -0,0 +1,33 @@
+import {
+  currentMonitor as getCurrentMonitor,
+  getCurrent as getCurrentWindow,
+} from '@tauri-apps/api/window';
+
+import { memoize } from '../utils';
+import { createResource } from 'solid-js';
+import { useDesktopCommands } from '../desktop';
+
+export const useConfigVariables = memoize(() => {
+  const commands = useDesktopCommands();
+
+  const [currentMonitor] = createResource(() => {
+    const currentMonitor = getCurrentMonitor();
+
+    if (!currentMonitor) {
+      commands.exitWithError('Unable to detect current monitor.');
+    }
+
+    return currentMonitor;
+  });
+
+  const [configVariables] = createResource(currentMonitor, currentMonitor => {
+    return {
+      screen_x: currentMonitor.position.x,
+      screen_y: currentMonitor.position.y,
+      screen_width: currentMonitor.size.width,
+      screen_height: currentMonitor.size.height,
+    };
+  });
+
+  return configVariables;
+});
