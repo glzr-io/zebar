9029892bcbcf983009621c9c5ff5b29071f58883
Lars Berger <lars.berger@accedo.tv>
2023-07-14 01:58:56 +0800

    refactor(fe): remove unused `delimitedKey` and `expandConfigKeys` utils

--- apps/frontend/src/app/shared/user-config/delimited-key.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export function delimitedKey<const T extends string>(prefix: T) {
-  return z.string().pipe(
-    z.custom<`${T}/${string}`>(val => {
-      return (val as string).startsWith(`${prefix}/`);
-    }),
-  );
-}
