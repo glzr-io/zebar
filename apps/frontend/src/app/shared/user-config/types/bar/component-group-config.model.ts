883a4752683e4f959adae91cc1d57d27ae37737e
Lars Berger <lars.berger@accedo.tv>
2023-07-14 01:57:15 +0800

    feat(fe): changes to user config validation

--- apps/frontend/src/app/shared/user-config/types/bar/component-group-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/component-group-config.model.ts
@@ -4,6 +4,7 @@ import { ComponentConfigSchema } from './component-config.model';
 import { ElementSchema } from '../shared/element.model';
 
 export const ComponentGroupConfigSchema = ElementSchema.extend({
+  class_name: z.string().default('group'),
   components: z.array(ComponentConfigSchema).default([]),
 });
 
