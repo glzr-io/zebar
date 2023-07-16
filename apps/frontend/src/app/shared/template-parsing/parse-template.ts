3fabf4c5e1281bf094a244733c7f9192f97a25a3
Lars Berger <lars.berger@yahoo.com>
2023-07-16 18:22:40 +0800

    refactor(fe): move `runTemplateEngine` fn to new file

--- apps/frontend/src/app/shared/template-parsing/parse-template.ts
+++ apps/frontend/src/app/shared/template-parsing/parse-template.ts
@@ -1,8 +1,7 @@
-import { renderString } from 'nunjucks';
-
+import { getBindingRegex } from './get-binding-regex';
+import { runTemplateEngine } from './run-template-engine';
 import { TemplateBindings } from './template-bindings.model';
 import { createUniqueId, insertAndReplace } from '../utils';
-import { getBindingRegex } from './get-binding-regex';
 
 export function parseTemplate(
   template: string,
@@ -41,46 +40,4 @@ export function parseTemplate(
   }
 
   return element.firstChild as Element;
-}
-
-/**
- * Nunjucks is used to evaluate variable + slot bindings in the template.
- */
-function runTemplateEngine(
-  template: string,
-  bindings: TemplateBindings,
-): string {
-  const {
-    variables = {},
-    slots = {},
-    functions = {},
-    components = {},
-  } = bindings;
-
-  const bindingsToEscape = [
-    ...Object.keys(functions),
-    ...Object.keys(components),
-  ];
-
-  // Need to ignore bindings that shouldn't be compiled by Nunjucks. Accomplish
-  // this by wrapping them in '{% raw %}' tag.
-  const escapedTemplate = template.replace(
-    getBindingRegex(bindingsToEscape, 'g'),
-    '{% raw %}$&{% endraw %}',
-  );
-
-  const compiledSlots = Object.keys(slots).reduce<Record<string, string>>(
-    (acc, slot) => {
-      return {
-        ...acc,
-        [slot]: renderString(slots[slot], variables),
-      };
-    },
-    {},
-  );
-
-  return renderString(escapedTemplate, {
-    ...variables,
-    slot: compiledSlots,
-  });
 }
