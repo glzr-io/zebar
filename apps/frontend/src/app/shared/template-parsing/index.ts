b5ef19be15294dc70540c1f1a1b2a1309faa1496
Lars Berger <lars.berger@accedo.tv>
2023-07-16 18:22:40 +0800

    refactor(fe): move `runTemplateEngine` fn to new file

--- apps/frontend/src/app/shared/template-parsing/index.ts
+++ apps/frontend/src/app/shared/template-parsing/index.ts
@@ -1,4 +1,5 @@
 export * from './create-template-element';
 export * from './get-binding-regex';
 export * from './parse-template';
+export * from './run-template-engine';
 export * from './template-bindings.model';
