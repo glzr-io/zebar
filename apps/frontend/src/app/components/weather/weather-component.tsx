eab8beac1168f4a7d9a581a5253378b7dd0335bb
Lars Berger <lars.berger@accedo.tv>
2023-07-16 18:20:02 +0800

    feat(fe): rename providers; be able to refresh providers

--- apps/frontend/src/app/components/weather/weather-component.tsx
+++ apps/frontend/src/app/components/weather/weather-component.tsx
@@ -3,18 +3,26 @@ import { createMemo } from 'solid-js';
 import defaultTemplate from './weather-component.njk?raw';
 import { createTemplateElement } from '~/shared/template-parsing';
 import { WeatherComponentConfig } from '~/shared/user-config';
-import { WeatherStatus, useWeather } from '~/shared/providers/use-weather.hook';
+import {
+  WeatherStatus,
+  useWeatherProvider,
+} from '~/shared/providers/use-weather-provider.hook';
 
 export function WeatherComponent(props: { config: WeatherComponentConfig }) {
-  const weather = useWeather(props.config.latitude, props.config.longitude);
+  const weatherProvider = useWeatherProvider(
+    props.config.latitude,
+    props.config.longitude,
+  );
 
   const bindings = createMemo(() => {
+    const weatherData = weatherProvider.data();
+
     return {
       variables: {
-        celsius_temp: weather()?.celsiusTemp ?? 0,
-        fahrenheit_temp: weather()?.fahrenheitTemp ?? 0,
-        wind_speed: weather()?.windSpeed ?? 0,
-        weather_status: weather()?.weatherStatus ?? WeatherStatus.CLEAR_DAY,
+        celsius_temp: weatherData?.celsiusTemp ?? 0,
+        fahrenheit_temp: weatherData?.fahrenheitTemp ?? 0,
+        wind_speed: weatherData?.windSpeed ?? 0,
+        weather_status: weatherData?.weatherStatus ?? WeatherStatus.CLEAR_DAY,
         WeatherStatus: WeatherStatus,
         root_props: `id="${props.config.id}" class="${props.config.class_name}"`,
       },
