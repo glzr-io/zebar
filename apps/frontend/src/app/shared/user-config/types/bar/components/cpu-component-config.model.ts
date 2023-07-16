443e5c45735dbef34efbc753fdb38409b9b8b431
Lars Berger <lars.berger@accedo.tv>
2023-07-16 16:14:56 +0800

    feat(fe): `useWeather` fetches and formats data from openmeteo

--- apps/frontend/src/app/shared/user-config/types/bar/components/cpu-component-config.model.ts
+++ apps/frontend/src/app/shared/user-config/types/bar/components/cpu-component-config.model.ts
@@ -5,7 +5,7 @@ import { ElementSchema } from '../../shared/element.model';
 export const CpuComponentConfigSchema = ElementSchema.extend({
   type: z.literal('cpu'),
   class_name: z.string().default('cpu-component'),
-  refresh_interval_ms: z.number().default(5000),
+  refresh_interval_ms: z.number().default(5 * 1000),
 });
 
 export type CpuComponentConfig = z.infer<typeof CpuComponentConfigSchema>;
