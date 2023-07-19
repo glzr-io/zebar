0ea4011fa8862fdede58f62dd45e22386a0a3445
Lars Berger <lars.berger@yahoo.com>
2023-07-20 03:56:33 +0800

    feat(fe): add debug setup for connecting to glazewm websocket server

--- apps/frontend/src/app/components/glazewm/glazewm-component.tsx
+++ apps/frontend/src/app/components/glazewm/glazewm-component.tsx
@@ -5,6 +5,34 @@ import { createTemplateElement } from '~/shared/template-parsing';
 import { GlazeWMComponentConfig } from '~/shared/user-config';
 
 export function GlazeWMComponent(props: { config: GlazeWMComponentConfig }) {
+  const socket = new WebSocket('ws://localhost:61423');
+
+  socket.onopen = function (e) {
+    console.log('[open] Connection established');
+    console.log('Sending to server');
+    socket.send('My name is John');
+  };
+
+  socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(
+        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
+      );
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error) {
+    console.log(`[error]`);
+  };
+
   const bindings = createMemo(() => {
     return {
       variables: {
