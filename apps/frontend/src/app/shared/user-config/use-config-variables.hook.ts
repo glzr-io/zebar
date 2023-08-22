d19e6909cc846cc2249d40323645654b34869a10
Lars Berger <lars.berger@accedo.tv>
2023-08-23 01:42:00 +0800

    feat(fe): use current monitor hook for getting screen-related config variables

--- apps/frontend/src/app/shared/user-config/use-config-variables.hook.ts
+++ apps/frontend/src/app/shared/user-config/use-config-variables.hook.ts
@@ -1,28 +1,19 @@
-import { currentMonitor as getCurrentMonitor } from '@tauri-apps/api/window';
-
-import { memoize } from '../utils';
 import { createResource } from 'solid-js';
-import { useDesktopCommands } from '../desktop';
+
+import { useCurrentMonitor } from '../desktop';
+import { memoize } from '../utils';
 
 export const useConfigVariables = memoize(() => {
-  const commands = useDesktopCommands();
+  const currentMonitor = useCurrentMonitor();
 
-  const [currentMonitor] = createResource(() => {
-    const currentMonitor = getCurrentMonitor();
+  const [configVariables] = createResource(async () => {
+    const monitorPosition = await currentMonitor.getPosition();
 
-    if (!currentMonitor) {
-      commands.exitWithError('Unable to detect current monitor.');
-    }
-
-    return currentMonitor;
-  });
-
-  const [configVariables] = createResource(currentMonitor, currentMonitor => {
     return {
-      screen_x: currentMonitor.position.x,
-      screen_y: currentMonitor.position.y,
-      screen_width: currentMonitor.size.width,
-      screen_height: currentMonitor.size.height,
+      screen_x: monitorPosition.x.toString(),
+      screen_y: monitorPosition.y.toString(),
+      screen_width: monitorPosition.width.toString(),
+      screen_height: monitorPosition.height.toString(),
     };
   });
 
