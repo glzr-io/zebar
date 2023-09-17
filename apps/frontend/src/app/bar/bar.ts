c0e077a0b12a61a8365bb1777d049c19a20853f9
Lars Berger <lars.berger@yahoo.com>
2023-09-17 12:47:05 +0800

    feat: create bar groups automatically from `group/**` keys in config

--- apps/frontend/src/app/bar/bar.ts
+++ apps/frontend/src/app/bar/bar.ts
@@ -2,28 +2,23 @@ import { createMemo } from 'solid-js';
 
 import defaultTemplate from './bar.njk?raw';
 import { createTemplateElement } from '~/shared/template-parsing';
-import { BarConfig } from '~/shared/user-config';
+import { BarConfig, GroupConfig } from '~/shared/user-config';
 import { Group } from '~/group/group';
 
 export function Bar(config: BarConfig): Element {
   const bindings = createMemo(() => ({
-    components: getGroupComponents(),
+    components: {
+      groups: getBarGroups,
+    },
   }));
 
-  // Create a map of components based on 'group/*' keys available in config.
-  // Map looks like `{ 'group.left': () => <Group ... /> }`.
-  function getGroupComponents() {
-    const groupNames = Object.keys(config)
-      .filter(key => key.startsWith('group/'))
-      .map(key => key.replace('group/', ''));
+  // Create an array of `Group` components based on 'group/**' keys in config.
+  function getBarGroups() {
+    const groupConfigs = Object.entries(config)
+      .filter(([key, value]) => key.startsWith('group') && !!value)
+      .map(([_, value]) => value) as GroupConfig[];
 
-    return groupNames.reduce<Record<string, () => Element>>(
-      (acc, name) => ({
-        ...acc,
-        [`group.${name}`]: () => Group(config[`group/${name}`]),
-      }),
-      {},
-    );
+    return groupConfigs.map(groupConfig => Group(groupConfig));
   }
 
   return createTemplateElement({
