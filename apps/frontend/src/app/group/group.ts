01b9325990dd18e28e215f4ba7c244e7a1c515c9
Lars Berger <lars.berger@yahoo.com>
2023-09-17 12:47:05 +0800

    feat: create bar groups automatically from `group/**` keys in config

--- apps/frontend/src/app/group/group.ts
+++ apps/frontend/src/app/group/group.ts
@@ -6,28 +6,32 @@ import { CpuComponent } from '~/components/cpu/cpu-component';
 import { GlazeWMComponent } from '~/components/glazewm/glazewm-component';
 import { WeatherComponent } from '~/components/weather/weather-component';
 import { createTemplateElement } from '~/shared/template-parsing';
-import { ComponentConfig, GroupConfig } from '~/shared/user-config';
+import { GroupConfig } from '~/shared/user-config';
 
 export function Group(config: GroupConfig): Element {
   const bindings = createMemo(() => ({
     components: {
-      components: () => config.components.map(getComponentType),
+      components: getBarComponents,
     },
   }));
 
-  function getComponentType(componentConfig: ComponentConfig) {
-    switch (componentConfig.type) {
-      case 'clock':
-        return ClockComponent(componentConfig);
-      case 'cpu':
-        return CpuComponent(componentConfig);
-      case 'glazewm':
-        return GlazeWMComponent(componentConfig);
-      case 'weather':
-        return WeatherComponent(componentConfig);
-      default:
-        throw new Error(`Unrecognized component type ${componentConfig.type}`);
-    }
+  function getBarComponents() {
+    return config.components.map(componentConfig => {
+      switch (componentConfig.type) {
+        case 'clock':
+          return ClockComponent(componentConfig);
+        case 'cpu':
+          return CpuComponent(componentConfig);
+        case 'glazewm':
+          return GlazeWMComponent(componentConfig);
+        case 'weather':
+          return WeatherComponent(componentConfig);
+        default:
+          throw new Error(
+            `Unrecognized component type ${componentConfig.type}`,
+          );
+      }
+    });
   }
 
   return createTemplateElement({
