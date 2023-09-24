24c1bfbd89f1b046a717a1d28f68ffef114ace78
Lars Berger <lars.berger@yahoo.com>
2023-09-24 17:20:05 +0800

    feat: create templates for glazewm workspaces + weather

--- apps/frontend/src/app/bar/bar-component.component.ts
+++ apps/frontend/src/app/bar/bar-component.component.ts
@@ -1,3 +1,7 @@
+import { createMemo } from 'solid-js';
+
+import glazewmWorkspacesTemplate from './glazewm-workspaces.template.njk?raw';
+import weatherTemplate from './weather.template.njk?raw';
 import { createTemplateElement } from '~/shared/template-parsing';
 import { ComponentConfig } from '~/shared/user-config';
 
@@ -6,6 +10,17 @@ export interface BarComponentProps {
 }
 
 export function BarComponent(props: BarComponentProps) {
+  const template = createMemo(() => {
+    switch (props.config.template) {
+      case 'template.glazewm_workspaces':
+        return glazewmWorkspacesTemplate;
+      case 'template.weather':
+        return weatherTemplate;
+      default:
+        return props.config.template;
+    }
+  });
+
   return createTemplateElement({
     bindings: () => ({}),
     config: () => props.config,
