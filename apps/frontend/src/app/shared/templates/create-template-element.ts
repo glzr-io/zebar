980d437e11af1ad20acd7acc5e1c0a374976942b
Lars Berger <lars.berger@yahoo.com>
2023-10-05 00:14:36 +0800

    refactor: simplify args to `createTemplateEngine`

--- apps/frontend/src/app/shared/templates/create-template-element.ts
+++ apps/frontend/src/app/shared/templates/create-template-element.ts
@@ -1,36 +1,33 @@
 import { Accessor, createEffect, onCleanup, onMount } from 'solid-js';
 
 import { useLogger } from '../logging';
-import { useTemplateEngine } from '../user-config';
 
 export interface CreateTemplateElementArgs {
-  id: Accessor<string>;
-  className: Accessor<string>;
-  variables: Record<string, unknown>;
+  id: string;
+  className: string;
   template: Accessor<string>;
 }
 
 export function createTemplateElement(args: CreateTemplateElementArgs) {
-  const templateEngine = useTemplateEngine();
-  const logger = useLogger(`.${args.className()}#${args.id()}`);
+  const logger = useLogger(`.${args.className}#${args.id}`);
 
   // Create element with ID.
   const element = document.createElement('div');
-  element.id = args.id();
+  element.id = args.id;
 
   createEffect(() => {
     // Create HTML element with the given template.
     const newElement = createRootElement();
     newElement.innerHTML = args.template();
 
-    const oldElement = document.getElementById(args.id());
+    const oldElement = document.getElementById(args.id);
     oldElement!.replaceWith(newElement);
   });
 
   function createRootElement() {
     const element = document.createElement('div');
-    element.id = args.id();
-    element.className = args.className();
+    element.id = args.id;
+    element.className = args.className;
     return element;
   }
 
