15471d2a5948348255974633d2232428ec7e2dd2
Lars Berger <lars.berger@yahoo.com>
2023-10-10 04:17:09 +0800

    feat: handle tokenizing tag blocks

--- apps/frontend/src/app/shared/template-engine/tokenize.ts
+++ apps/frontend/src/app/shared/template-engine/tokenize.ts
@@ -29,67 +29,92 @@ export function tokenize(template: string): Token[] {
 
     switch (state) {
       case TokenizerState.DEFAULT: {
-        if (scanner.scan(/@if/)) {
-          pushToken(TokenType.IF_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/@else\s+if/)) {
-          pushToken(TokenType.ELSE_IF_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/@else/)) {
-          pushToken(TokenType.ELSE_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/@for/)) {
-          pushToken(TokenType.FOR_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/@switch/)) {
-          pushToken(TokenType.SWITCH_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/@case/)) {
-          pushToken(TokenType.CASE_TAG);
-          stateStack.push(TokenizerState.IN_TAG_PARAMS);
-        } else if (scanner.scan(/{{/)) {
-          pushToken(TokenType.OPEN_INTERPOLATION_TAG);
-          stateStack.push(TokenizerState.IN_INTERPOLATION);
-        } else if (scanner.scanUntil(/.*?(?={{|@)/)) {
-          // Search until the start of a tag or interpolation.
-          pushToken(TokenType.TEXT);
-        } else {
-          pushToken(TokenType.TEXT);
-          scanner.terminate();
-        }
-        break;
-      }
-      case TokenizerState.IN_INTERPOLATION: {
-        if (scanner.scan(/\s+/)) {
-          // Ignore whitespace within interpolation tag.
-        } else if (scanner.scan(/}}/)) {
-          pushToken(TokenType.CLOSE_INTERPOLATION_TAG);
-          stateStack.pop();
-        } else if (scanner.scan(/.*?(?=}})/)) {
-          // Match expression until closing `}}`.
-          pushToken(TokenType.EXPRESSION);
-        } else {
-          console.log('bad output', scanner, tokens, stateStack, template);
-          throw new TemplateError('aa', scanner.getCursor());
-        }
+        tokenizeDefault();
         break;
       }
       case TokenizerState.IN_TAG_PARAMS: {
-        if (scanner.scan(/\s+/)) {
-          // Ignore whitespace within tag params.
-        } else if (scanner.scan(/{/)) {
-          pushToken(TokenType.OPEN_TAG_BLOCK);
-          stateStack.pop();
-          stateStack.push(TokenizerState.IN_TAG_BLOCK);
-        } else if (scanner.scan(/\((.*?)\)/)) {
-          // TODO: Need to ignore nested parenthesis within tag params.
-          pushToken(TokenType.EXPRESSION);
-        } else {
-          console.log('bad tag start', scanner, tokens, stateStack, template);
-          throw new TemplateError('aa', scanner.getCursor());
-        }
+        tokenizeTagParams();
         break;
       }
+      case TokenizerState.IN_TAG_BLOCK: {
+        tokenizeTagBlock();
+        break;
+      }
+      case TokenizerState.IN_INTERPOLATION: {
+        tokenizeInterpolation();
+        break;
+      }
+    }
+  }
+
+  function tokenizeDefault() {
+    if (scanner.scan(/@if/)) {
+      pushToken(TokenType.IF_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/@else\s+if/)) {
+      pushToken(TokenType.ELSE_IF_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/@else/)) {
+      pushToken(TokenType.ELSE_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/@for/)) {
+      pushToken(TokenType.FOR_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/@switch/)) {
+      pushToken(TokenType.SWITCH_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/@case/)) {
+      pushToken(TokenType.CASE_TAG);
+      stateStack.push(TokenizerState.IN_TAG_PARAMS);
+    } else if (scanner.scan(/{{/)) {
+      pushToken(TokenType.OPEN_INTERPOLATION_TAG);
+      stateStack.push(TokenizerState.IN_INTERPOLATION);
+    } else if (scanner.scanUntil(/.*?(?={{|@)/)) {
+      // Search until the start of a tag or interpolation.
+      pushToken(TokenType.TEXT);
+    } else {
+      pushToken(TokenType.TEXT);
+      scanner.terminate();
+    }
+  }
+
+  function tokenizeTagParams() {
+    if (scanner.scan(/\s+/)) {
+      // Ignore whitespace within tag params.
+    } else if (scanner.scan(/{/)) {
+      pushToken(TokenType.OPEN_TAG_BLOCK);
+      stateStack.pop();
+      stateStack.push(TokenizerState.IN_TAG_BLOCK);
+    } else if (scanner.scan(/\((.*?)\)/)) {
+      // TODO: Need to ignore nested parenthesis within tag params.
+      pushToken(TokenType.EXPRESSION);
+    } else {
+      console.log('bad tag start', scanner, tokens, stateStack, template);
+      throw new TemplateError('aa', scanner.getCursor());
+    }
+  }
+
+  function tokenizeTagBlock() {
+    if (scanner.scan(/}/)) {
+      pushToken(TokenType.CLOSE_TAG_BLOCK);
+      stateStack.pop();
+    } else {
+      tokenizeDefault();
+    }
+  }
+
+  function tokenizeInterpolation() {
+    if (scanner.scan(/\s+/)) {
+      // Ignore whitespace within interpolation tag.
+    } else if (scanner.scan(/}}/)) {
+      pushToken(TokenType.CLOSE_INTERPOLATION_TAG);
+      stateStack.pop();
+    } else if (scanner.scan(/.*?(?=}})/)) {
+      // Match expression until closing `}}`.
+      pushToken(TokenType.EXPRESSION);
+    } else {
+      console.log('bad output', scanner, tokens, stateStack, template);
+      throw new TemplateError('aa', scanner.getCursor());
     }
   }
 
