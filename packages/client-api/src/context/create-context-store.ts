4c6c631f24d52fd6c6c815d63d362670e4fbae3f
Lars Berger <lars.berger@yahoo.com>
2023-10-25 20:14:19 +0800

    wip `createContextStore`

--- packages/client-api/src/context/create-context-store.ts
+++ packages/client-api/src/context/create-context-store.ts
@@ -1,13 +1,80 @@
-import { createComputed } from 'solid-js';
+import { createComputed, createMemo } from 'solid-js';
 import { createStore } from 'solid-js/store';
 
-export function createContextStore(config: unknown) {
-  const [contextTree, setContextTree] = createStore({});
+import {
+  BarConfigSchemaP1,
+  BaseElementConfig,
+  ProvidersConfigSchema,
+  UserConfig,
+  getBarConfigEntries,
+} from '~/user-config';
+import { createProvider } from './providers';
+import { parseConfigSection } from '~/user-config/parse-config-section';
+import { ElementContext } from './element-context.model';
+
+export function createContextStore(
+  config: UserConfig,
+  configVariables: () => Record<string, unknown>,
+) {
+  const [contextTree, setContextTree] = createStore<ElementContext>(
+    {} as ElementContext,
+  );
 
   createComputed(createContextTree);
 
   function createContextTree() {
-    const barConfigs = [];
+    const windowId = 'bar';
+    const windowConfig = config[`bar/${windowId}`];
+
+    const rootVariables = createMemo(() => ({
+      env: configVariables()!,
+    }));
+
+    const windowVariables = createMemo(() => ({
+      ...rootVariables(),
+      ...getElementVariables(windowConfig),
+    }));
+
+    createComputed(() => {
+      const parsedConfig = parseConfigSection(
+        { ...windowConfig, id: windowId },
+        BarConfigSchemaP1.strip(),
+        windowVariables(),
+      );
+
+      setContextTree(windowId, parsedConfig);
+    });
+  }
+
+  function createElementContext(
+    config: BaseElementConfig,
+    parentContext: ElementContext,
+  ): ElementContext {
+    const id = parentContext ? `${parentContext.id}-${config.id}` : config.id;
+
+    return {
+      id,
+      parent: parentContext,
+      children: '',
+      rawConfig: '',
+      parsedConfig: '',
+      data: '',
+    };
+  }
+
+  // TODO: Get variables from `variables` config as well.
+  function getElementVariables(config: BaseElementConfig) {
+    const providerConfigs = ProvidersConfigSchema.parse(
+      config?.providers ?? [],
+    );
+
+    return providerConfigs.reduce(
+      (acc, config) => ({
+        ...acc,
+        [config.type]: createProvider(config).variables,
+      }),
+      {},
+    );
   }
 
   return {
