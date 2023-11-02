f257e706a2263a3be980f2d7919464f157942acf
Lars Berger <lars.berger@yahoo.com>
2023-11-02 23:01:07 +0800

    feat: move out fns for creating channels

--- packages/desktop/src/providers/provider_manager.rs
+++ packages/desktop/src/providers/provider_manager.rs
@@ -5,7 +5,6 @@ use tokio::sync::{
   Mutex,
 };
 
-use anyhow::Result;
 use tauri::{Manager, Runtime};
 use tokio::{sync::mpsc, task};
 use tracing::info;
@@ -21,6 +20,10 @@ pub struct CreateProviderArgs {
   pub options_hash: String,
   pub options: ProviderConfig,
   pub tracked_access: Vec<String>,
+}
+
+pub struct UnlistenProviderArgs {
+  pub options_hash: String,
 }
 
 /// Handles the creation and deletion of providers.
@@ -39,26 +42,82 @@ pub fn init<R: Runtime>(app: &mut tauri::App<R>) -> tauri::plugin::Result<()> {
   Ok(())
 }
 
-// fn create_manager() -> ProviderManager {
-//   let (input_sender, input_receiver) = mpsc::channel(1);
-//   let (output_sender, mut output_receiver) = mpsc::channel(1);
+fn handle_provider_emit_output<R: tauri::Runtime>(
+  // output_receiver: &mut Receiver<String>,
+  manager: (impl Manager<R> + std::marker::Sync + std::marker::Send + 'static),
+  // manager: &impl Manager<R>,
+) -> (Sender<String>) {
+  let (output_sender, mut output_receiver) = mpsc::channel(1);
 
-// }
+  task::spawn(async move {
+    while let Some(output) = output_receiver.recv().await {
+      info!(?output, "handle_provider_emit");
+      manager.emit_all("provider-emit", output).unwrap();
+    }
+    // loop {
+    //   if let Some(output) = output_receiver.recv().await {
+    //     info!(?output, "handle_provider_emit");
+    //     manager.emit_all("provider-emit", output).unwrap();
+    //   }
+    // }
+  });
 
-fn handle_provider_create() -> Sender<String> {}
+  output_sender
+}
+
+fn handle_provider_listen_input(
+  output_sender: Sender<String>,
+) -> Sender<CreateProviderArgs> {
+  let (input_sender, mut input_receiver) =
+    mpsc::channel::<CreateProviderArgs>(1);
+  // let (output_sender, mut output_receiver) = mpsc::channel(1);
+
+  task::spawn(async move {
+    while let Some(input) = input_receiver.recv().await {
+      let provider: Arc<Mutex<dyn Provider + Send + Sync + 'static>> =
+        match input.options {
+          ProviderConfig::Cpu(config) => {
+            Arc::new(Mutex::new(CpuProvider::new(config)))
+          }
+          ProviderConfig::Network(config) => {
+            Arc::new(Mutex::new(NetworkProvider::new(config)))
+          }
+        };
+
+      let sender = output_sender.clone();
+      let provider_clone = provider.clone();
+
+      task::spawn(async move {
+        let provider = provider_clone.lock().await;
+        provider.start(sender).await; // Starts a long-running task
+      });
+
+      // self.active_providers.push(provider);
+    }
+  });
+
+  input_sender
+}
+
+fn handle_provider_unlisten_input(
+  output_sender: Sender<String>,
+) -> Sender<UnlistenProviderArgs> {
+  let (input_sender, input_receiver) = mpsc::channel::<UnlistenProviderArgs>(1);
+
+  input_sender
+}
 
 impl ProviderManager {
   pub fn new<R: Runtime>(app: tauri::AppHandle<R>) -> ProviderManager {
-    let (input_sender, input_receiver) = mpsc::channel(1);
-    let (output_sender, mut output_receiver) = mpsc::channel(1);
+    // let (input_sender, input_receiver) = mpsc::channel(1);
+    // let (output_sender, mut output_receiver) = mpsc::channel(1);
+    let (output_sender) = handle_provider_emit_output(app.app_handle());
 
-    task::spawn(async move {
-      Self::handle_provider_create(input_receiver, output_sender).await
-    });
+    let (input_sender) = handle_provider_listen_input(output_sender);
 
-    task::spawn(async move {
-      Self::handle_provider_emit(&mut output_receiver, &app.app_handle()).await
-    });
+    // task::spawn(async move {
+    //   Self::handle_provider_emit(&mut output_receiver, &app.app_handle()).await
+    // });
 
     ProviderManager {
       input_sender,
@@ -116,51 +175,50 @@ impl ProviderManager {
   //   // self
   // }
 
-  async fn handle_provider_create(
-    &mut self,
-    mut input_receiver: mpsc::Receiver<CreateProviderArgs>,
-    output_sender: mpsc::Sender<String>,
-    // ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-  ) {
-    while let Some(input) = input_receiver.recv().await {
-      let provider: Arc<Mutex<dyn Provider + Send + Sync + 'static>> =
-        match input.options {
-          ProviderConfig::Cpu(config) => {
-            Arc::new(Mutex::new(CpuProvider::new(config)))
-          }
-          ProviderConfig::Network(config) => {
-            Arc::new(Mutex::new(NetworkProvider::new(config)))
-          }
-          _ => panic!(),
-        };
+  // async fn handle_provider_create(
+  //   &mut self,
+  //   mut input_receiver: mpsc::Receiver<CreateProviderArgs>,
+  //   output_sender: mpsc::Sender<String>,
+  //   // ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+  // ) {
+  //   while let Some(input) = input_receiver.recv().await {
+  //     let provider: Arc<Mutex<dyn Provider + Send + Sync + 'static>> =
+  //       match input.options {
+  //         ProviderConfig::Cpu(config) => {
+  //           Arc::new(Mutex::new(CpuProvider::new(config)))
+  //         }
+  //         ProviderConfig::Network(config) => {
+  //           Arc::new(Mutex::new(NetworkProvider::new(config)))
+  //         }
+  //       };
 
-      let sender = output_sender.clone();
-      let provider_clone = provider.clone();
+  //     let sender = output_sender.clone();
+  //     let provider_clone = provider.clone();
 
-      task::spawn(async move {
-        let provider = provider_clone.lock().await;
-        provider.start(sender).await; // Starts a long-running task
-      });
+  //     task::spawn(async move {
+  //       let provider = provider_clone.lock().await;
+  //       provider.start(sender).await; // Starts a long-running task
+  //     });
 
-      self.active_providers.push(provider);
-    }
-  }
+  //     self.active_providers.push(provider);
+  //   }
+  // }
 
-  async fn on_delete(&self) {
-    let provider = self.active_providers.get(1).unwrap().lock().await;
-    provider.stop();
-  }
+  // async fn on_delete(&self) {
+  //   let provider = self.active_providers.get(1).unwrap().lock().await;
+  //   provider.stop();
+  // }
 
-  async fn handle_provider_emit<R: tauri::Runtime>(
-    &self,
-    output_receiver: &mut Receiver<String>,
-    manager: &impl Manager<R>,
-  ) {
-    loop {
-      if let Some(output) = output_receiver.recv().await {
-        info!(?output, "handle_provider_emit");
-        manager.emit_all("provider-emit", output).unwrap();
-      }
-    }
-  }
+  // async fn handle_provider_emit<R: tauri::Runtime>(
+  //   &self,
+  //   output_receiver: &mut Receiver<String>,
+  //   manager: &impl Manager<R>,
+  // ) {
+  //   loop {
+  //     if let Some(output) = output_receiver.recv().await {
+  //       info!(?output, "handle_provider_emit");
+  //       manager.emit_all("provider-emit", output).unwrap();
+  //     }
+  //   }
+  // }
 }
