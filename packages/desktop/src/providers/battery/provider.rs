2509e4feb8cc207a34ce192661d3c3e528de7528
Lars Berger <lars.berger@yahoo.com>
2023-11-09 04:58:28 +0800

    feat: wip `RefreshableProvider`

--- packages/desktop/src/providers/battery/provider.rs
+++ packages/desktop/src/providers/battery/provider.rs
@@ -1,18 +1,16 @@
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use async_trait::async_trait;
-use starship_battery::{
-  units::{power::watt, ratio::percent, time::second},
-  Battery, Manager,
-};
+use starship_battery::Manager;
 use sysinfo::System;
 use tokio::{
   sync::{mpsc::Sender, Mutex},
-  task::{self, AbortHandle},
-  time,
+  task::AbortHandle,
 };
 
-use crate::providers::{provider::Provider, variables::ProviderVariables};
+use crate::providers::{
+  provider::RefreshableProvider, variables::ProviderVariables,
+};
 
 use super::{BatteryProviderConfig, BatteryVariables};
 
@@ -33,9 +31,28 @@ impl BatteryProvider {
       sysinfo,
     }
   }
+}
+
+#[async_trait]
+impl RefreshableProvider<System> for BatteryProvider {
+  fn refresh_interval_ms(&self) -> u64 {
+    self.config.refresh_interval_ms
+  }
+
+  fn state(&self) -> Mutex<System> {
+    self.sysinfo.clone()
+  }
+
+  fn abort_handle(&self) -> Option<AbortHandle> {
+    self.abort_handle
+  }
+
+  fn set_abort_handle(&self, abort_handle: AbortHandle) {
+    self.abort_handle = Some(abort_handle)
+  }
 
   async fn refresh_and_emit(
-    sysinfo: &Mutex<System>,
+    sysinfo: &Mutex<T>,
     emit_output_tx: &Sender<ProviderVariables>,
   ) {
     let manager = Manager::new().unwrap();
@@ -56,38 +73,5 @@ impl BatteryProvider {
     _ = emit_output_tx
       .send(ProviderVariables::Battery(BatteryVariables {}))
       .await;
-  }
-}
-
-#[async_trait]
-impl Provider for BatteryProvider {
-  async fn on_start(&mut self, emit_output_tx: Sender<ProviderVariables>) {
-    let refresh_interval_ms = self.config.refresh_interval_ms;
-    let sysinfo = self.sysinfo.clone();
-
-    let forever = task::spawn(async move {
-      let mut interval =
-        time::interval(Duration::from_millis(refresh_interval_ms));
-
-      Self::refresh_and_emit(&sysinfo, &emit_output_tx).await;
-
-      loop {
-        interval.tick().await;
-        Self::refresh_and_emit(&sysinfo, &emit_output_tx).await;
-      }
-    });
-
-    self.abort_handle = Some(forever.abort_handle());
-    _ = forever.await;
-  }
-
-  async fn on_refresh(&mut self, emit_output_tx: Sender<ProviderVariables>) {
-    Self::refresh_and_emit(&self.sysinfo, &emit_output_tx).await;
-  }
-
-  async fn on_stop(&mut self) {
-    if let Some(handle) = &self.abort_handle {
-      handle.abort();
-    }
   }
 }
