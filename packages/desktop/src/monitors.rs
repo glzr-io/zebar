ac1396d0ae353d2129e58f23be5e4bcc736573c0
Lars Berger <lars.berger@yahoo.com>
2023-11-20 20:58:31 +0800

    feat: replace newlines with \0 with `tr`; wrap monitor output in quotes

--- packages/desktop/src/monitors.rs
+++ packages/desktop/src/monitors.rs
@@ -7,15 +7,9 @@ pub fn output_monitors<R: Runtime>(app: &mut App<R>) -> Result<()> {
     .context("Unable to detect monitors")?;
 
   for monitor in monitors {
-    // Get monitor name with escaped spaces.
-    let monitor_name = monitor
-      .name()
-      .context("Unable to read monitor name")?
-      .replace(' ', "\\ ");
-
     println!(
-      "MONITOR_NAME={} MONITOR_X={} MONITOR_Y={} MONITOR_WIDTH={} MONITOR_HEIGHT={}",
-      monitor_name,
+      "MONITOR_NAME=\"{}\" MONITOR_X=\"{}\" MONITOR_Y=\"{}\" MONITOR_WIDTH=\"{}\" MONITOR_HEIGHT=\"{}\"",
+      monitor.name().context("Unable to read monitor name")?,
       monitor.position().x,
       monitor.position().y,
       monitor.size().width,
