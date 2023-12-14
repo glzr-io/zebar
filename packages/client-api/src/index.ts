282277cb5c8784d9e6d108b746bd8c798ae09d4d
Lars Berger <lars.berger@yahoo.com>
2023-12-15 02:58:18 +0800

    refactor: remove `context` dir

--- packages/client-api/src/index.ts
+++ packages/client-api/src/index.ts
@@ -1,3 +1,4 @@
-export { type ElementContext, ElementType } from './context';
+export { type ElementContext } from './element-context.model';
+export { ElementType } from './element-type.model';
 export { createLogger, toCssSelector } from './utils';
 export { initWindow, initWindowAsync } from './init-window';
