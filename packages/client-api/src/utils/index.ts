80132ef72b0ca17d3dfa6bb6561f1f4e908c0edf
Lars Berger <lars.berger@yahoo.com>
2023-12-16 02:11:57 +0800

    refactor: remove unused util `getRandomWithoutCollision`

--- packages/client-api/src/utils/index.ts
+++ packages/client-api/src/utils/index.ts
@@ -3,6 +3,5 @@ export * from './clsx';
 export * from './create-getter-proxy';
 export * from './create-logger';
 export * from './create-string-scanner';
-export * from './get-random-without-collision';
 export * from './simple-hash';
 export * from './to-css-selector';
