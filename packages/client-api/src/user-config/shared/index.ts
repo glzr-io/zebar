3788ffbd5d0704b07ced877f9c3dde0a7a422874
Lars Berger <lars.berger@yahoo.com>
2023-12-23 17:39:22 +0000

    refactor: remove unused variable configs

--- packages/client-api/src/user-config/shared/index.ts
+++ packages/client-api/src/user-config/shared/index.ts
@@ -1,7 +1,5 @@
 export * from './boolean-like.model';
 export * from './format-config-error';
 export * from './get-child-ids';
-export * from './http-variable-config.model';
-export * from './script-variable-config.model';
 export * from './template-property-error';
 export * from './with-dynamic-key';
