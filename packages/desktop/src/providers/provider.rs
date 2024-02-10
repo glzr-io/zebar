bf3525426dcd4d2d36efba4e368c7f506ef28fb2
Lars Berger <lars.berger@yahoo.com>
2024-02-10 16:19:16 +0800

    fix: prevent running `on_start` repeatedly on provider refresh

--- packages/desktop/src/providers/provider.rs
+++ packages/desktop/src/providers/provider.rs
@@ -27,17 +27,29 @@ pub trait Provider {
     mut refresh_rx: Receiver<()>,
     mut stop_rx: Receiver<()>,
   ) {
+    let mut has_started = false;
+
     // Loop to avoid exiting the select on refresh.
     loop {
       let config_hash = config_hash.clone();
       let emit_output_tx = emit_output_tx.clone();
 
       tokio::select! {
-        _ = self.on_start(config_hash.clone(), emit_output_tx.clone()) => break,
+        // Default match arm which handles initialization of the provider.
+        // This has a precondition to avoid running again on refresh.
+        _ = {
+          info!("Starting provider: {}", config_hash);
+          has_started = true;
+          self.on_start(config_hash.clone(), emit_output_tx.clone())
+        }, if !has_started => break,
+
+        // On refresh, re-emit provider variables and continue looping.
         Some(_) = refresh_rx.recv() => {
           info!("Refreshing provider: {}", config_hash);
           _ = self.on_refresh(config_hash, emit_output_tx).await;
         },
+
+        // On stop, perform any necessary clean up and exit the loop.
         Some(_) = stop_rx.recv() => {
           info!("Stopping provider: {}", config_hash);
           _ = self.on_stop().await;
