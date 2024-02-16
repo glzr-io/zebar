0da0117fda8db2d7294adaf10392df65db717d99
Lars Berger <lars.berger@yahoo.com>
2024-02-17 04:22:13 +0800

    feat: return time till full + empty in milliseconds

--- packages/desktop/src/providers/battery/provider.rs
+++ packages/desktop/src/providers/battery/provider.rs
@@ -4,7 +4,8 @@ use anyhow::{Context, Result};
 use async_trait::async_trait;
 use starship_battery::{
   units::{
-    electric_potential::volt, power::watt, ratio::percent, time::second,
+    electric_potential::volt, power::watt, ratio::percent,
+    time::millisecond,
   },
   Manager, State,
 };
@@ -49,10 +50,10 @@ impl BatteryProvider {
       is_charging: battery.state() == State::Charging,
       time_till_full: battery
         .time_to_full()
-        .map(|time| time.get::<second>()),
+        .map(|time| time.get::<millisecond>()),
       time_till_empty: battery
         .time_to_empty()
-        .map(|time| time.get::<second>()),
+        .map(|time| time.get::<millisecond>()),
       power_consumption: battery.energy_rate().get::<watt>(),
       voltage: battery.voltage().get::<volt>(),
       cycle_count: battery.cycle_count(),
