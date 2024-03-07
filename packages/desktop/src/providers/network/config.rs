1eb5c3437b580e65349c23e2f18b10e7985c047d
matthewtapps <matt.tapps@gmail.com>
2024-03-08 10:06:43 +1100

    Rename one more file

--- packages/desktop/src/providers/network/config.rs
+++ packages/desktop/src/providers/network/config.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 use crate::impl_interval_config;
 
 #[derive(Deserialize, Debug)]
-#[serde(tag = "type", rename = "newnetwork")]
+#[serde(tag = "type", rename = "network")]
 pub struct NetworkProviderConfig {
   pub refresh_interval: u64,
 }
