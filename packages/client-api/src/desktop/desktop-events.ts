d73d9f178d76308f09f7351c4a49b8e9651a349d
Lars Berger <lars.berger@yahoo.com>
2024-03-10 14:08:08 +0800

    fix: prevent duplicate subscriptions to provider emissions

--- packages/client-api/src/desktop/desktop-events.ts
+++ packages/client-api/src/desktop/desktop-events.ts
@@ -13,14 +13,45 @@ export interface ProviderEmitEvent<T = unknown> {
   variables: { data: T } | { error: string };
 }
 
+let listenPromise: Promise<UnlistenFn> | null = null;
+
+let callbacks: {
+  configHash: string;
+  fn: (payload: Event<ProviderEmitEvent<any>>) => void;
+}[] = [];
+
 /**
  * Listen for provider data.
  */
-export function onProviderEmit<T = unknown>(
+export async function onProviderEmit<T = unknown>(
   configHash: string,
   callback: (payload: T) => void,
 ): Promise<UnlistenFn> {
-  return listen('provider-emit', (event: Event<ProviderEmitEvent<T>>) => {
+  registerEventCallback(configHash, callback);
+
+  const unlisten = await (listenPromise ??
+    (listenPromise = listenProviderEmit()));
+
+  // Unlisten when there are no active callbacks.
+  return () => {
+    callbacks = callbacks.filter(
+      callback => callback.configHash !== configHash,
+    );
+
+    if (callbacks.length === 0) {
+      unlisten();
+    }
+  };
+}
+
+/**
+ * Add callback to invoke when a provider emits data.
+ */
+function registerEventCallback<T>(
+  configHash: string,
+  callback: (payload: T) => void,
+) {
+  const wrappedCallback = (event: Event<ProviderEmitEvent<T>>) => {
     // Ignore provider emissions for different configs.
     if (event.payload.configHash !== configHash) {
       return;
@@ -35,5 +66,22 @@ export function onProviderEmit<T = unknown>(
 
     logger.debug('Incoming provider variables:', variables.data);
     callback(variables.data as T);
+  };
+
+  callbacks.push({ configHash, fn: wrappedCallback });
+}
+
+/**
+ * Create listener for provider emissions.
+ *
+ * Only one Tauri event listener is needed to listen to all providers.
+ */
+async function listenProviderEmit(): Promise<UnlistenFn> {
+  return listen('provider-emit', (event: Event<ProviderEmitEvent>) => {
+    callbacks.forEach(callback => {
+      if (event.payload.configHash === callback.configHash) {
+        callback.fn(event);
+      }
+    });
   });
 }
