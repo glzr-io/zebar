d41050167ce2814c5dbd9395349931f6a0b3f0b8
Csaba <georgelisabeth1231@gmail.com>
2024-04-20 15:17:53 +0200

    data provider convertBytes

--- packages/client-api/src/providers/data/create-data-provider.ts
+++ packages/client-api/src/providers/data/create-data-provider.ts
@@ -1,4 +1,4 @@
-import { type Owner, onCleanup, runWithOwner } from 'solid-js';
+import { type Owner } from 'solid-js';
 import { createStore } from 'solid-js/store';
 
 import type { DataProviderConfig } from '~/user-config';
@@ -12,88 +12,54 @@ export async function createDataProvider(
   const [dataVariables, setDataVariables] =
     createStore<DataVariables>(getDataVariables());
 
-  const interval = setInterval(
-    () => setDataVariables(getDataVariables()),
-    config.refresh_interval,
-  );
-
-  runWithOwner(owner, () => {
-    onCleanup(() => clearInterval(interval));
-  });
-
   function getDataVariables() {
     return {};
   }
 
-  function toPrettyBytes(bytes: number, decimals = 2) {
-    const k = 1024;
-    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const bitUnits = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+  const byteCommonUnits = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const byteIECUnits = [' B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
-    return formatBytes(k, sizes, bytes, decimals);
+  function convertBytes(bytes: number, decimals = 2, minUnit: 'B') {
+    let unitIndex = bitUnits.findIndex(u => u === minUnit);
+
+    if (unitIndex !== -1) {
+      bytes *= 8;
+      return convert(1000, bitUnits, bytes, decimals, unitIndex);
+    }
+
+    unitIndex = byteCommonUnits.findIndex(u => u === minUnit);
+
+    if (unitIndex !== -1) {
+      return convert(1000, byteCommonUnits, bytes, decimals, unitIndex);
+    }
+
+    unitIndex = byteIECUnits.findIndex(u => u === minUnit);
+
+    if (unitIndex !== -1) {
+      return convert(1024, byteIECUnits, bytes, decimals, unitIndex);
+    }
+
+    return 'NoUnit';
   }
 
-  function toPrettyBits(bytes: number, decimals = 2) {
-    const k = 1000;
-    const sizes = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
-
-    bytes *= 8;
-
-    return formatBytes(k, sizes, bytes, decimals);
-  }
-
-  function formatBytes(k: number, sizes: string[], bytes: number, decimals = 2) {
+  function convert(k: number, units: string[], bytes: number, decimals = 2, unitIndex: number) {
     const dm = decimals < 0 ? 0 : decimals;
 
-    if (!+bytes)
-      return `${0.0.toFixed(dm)} ${sizes[0]}`;
+    if (!+bytes) {
+      return `${0.0.toFixed(dm)} ${units[unitIndex]}`;
+    }
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    let i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+    if (i < unitIndex) {
+      i = unitIndex;
+    }
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${units[i]?.trimStart()}`;
   }
 
   return {
-    toPrettyBytes,
-    toPrettyBits,
+    convertBytes,
   };
 }
-
-/*
-fn to_pretty_bytes(input_in_bytes: u64, timespan_in_ms: u64) -> String {   
-    let seconds = timespan_in_ms as f32 / f32::powf(10.0, 3.0);
-    let input = input_in_bytes as f32 / seconds;
-
-    if input < 1024.0 {
-        return format!("0.0 KB");
-    }
-
-    let magnitude = input.log(1024 as f32) as u32;
-    let base: Option<DataUnit> = num::FromPrimitive::from_u32(magnitude);
-    let result = input as f32 / ((1 as u64) << (magnitude * 10)) as f32;
-    
-    match base {
-        Some(DataUnit::B) => format!("{result:.1} B"),
-        Some(unit) => format!("{result:.1} {unit}B"),
-        None => format!("Unknown data unit"),
-    }
-}
-
-fn to_pretty_bits(input_in_bytes: u64, timespan_in_ms: u64) -> String {
-    if input_in_bytes < 1000 {
-        return format!("0.0 Kb");
-    }
-
-    let input = input_in_bytes * 8;
-
-    let seconds = timespan_in_ms as f32 / f32::powf(10.0, 3.0);
-    let magnitude = input.ilog(1000);
-    let base: Option<DataUnit> = num::FromPrimitive::from_u32(magnitude);
-    let result = (input as f32 / seconds) / f32::powf(1000.0, magnitude as f32);
-
-    match base {
-        Some(DataUnit::B) => format!("{result:.2} b"),
-        Some(unit) => format!("{result:.2} {unit}b"),
-        None => format!("Unknown data unit"),
-    }
-}
-*/
