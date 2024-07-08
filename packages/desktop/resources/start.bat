1809b0b8af8b20c71e6670b0b5274344d88ed703
Lars Berger <lars.berger@yahoo.com>
2024-07-08 11:36:10 +0800

    feat: hidden window style in default `start.bat` script

--- packages/desktop/resources/start.bat
+++ packages/desktop/resources/start.bat
@@ -2,4 +2,4 @@
 @REM Start hidden powershell script, which runs `zebar open bar --args ...` for every monitor.
 powershell -WindowStyle hidden -Command ^
   $monitors = zebar monitors; ^
-  foreach ($monitor in $monitors) { Start-Process -NoNewWindow -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
+  foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
