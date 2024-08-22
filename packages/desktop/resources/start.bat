6695a30d100f2fea0b204cdbb73385411612bf5d
Lars Berger <lars.berger@yahoo.com>
2024-08-22 18:24:43 +0800

    feat: remove start script

--- packages/desktop/resources/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@REM Start hidden powershell script, which runs `zebar open bar --args ...` for every monitor.
-powershell -WindowStyle hidden -Command ^
-  $monitors = zebar monitors; ^
-  foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
