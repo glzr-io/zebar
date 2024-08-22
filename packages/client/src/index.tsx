707e1123a71d7e6e79aa69700640152d6c46727f
Lars Berger <lars.berger@yahoo.com>
2024-08-22 19:47:45 +0800

    refactor: remove client app

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,14 +1,3 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
-
 import './normalize.css';
 import './index.css';
-import { WindowElement } from './app/window-element.component';
-
-const root = document.getElementById('zebar');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error('Root element not found.');
-}
-
-render(() => <WindowElement />, root!);
