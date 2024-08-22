glzr-io/zebar

commit 8e585faeef157c762c32cbb4c515b1bdcafa2626

refactor: simplify provider initialization and cleanup (#84)

packages/desktop/src/providers/provider_ref.rs
@@ -1,0 +1,202 @@
+use std::time::{Duration, Instant};
+
+use anyhow::bail;
+use serde::Serialize;
+use tokio::{sync::mpsc, task};
+use tracing::info;
+
+#[cfg(windows)]
+use super::komorebi::KomorebiProvider;
+use super::{
+  battery::BatteryProvider, config::ProviderConfig, cpu::CpuProvider,
+  host::HostProvider, ip::IpProvider, memory::MemoryProvider,
+  network::NetworkProvider, provider::Provider,
+  provider_manager::SharedProviderState, variables::ProviderVariables,
+  weather::WeatherProvider,
+};
+
+/// Reference to an active provider.
+#[derive(Debug, Clone)]
+pub struct ProviderRef {
+  pub config_hash: String,
+  pub min_refresh_interval: Option<Duration>,
+  pub cache: Option<ProviderCache>,
+  pub emit_output_tx: mpsc::Sender<ProviderOutput>,
+  pub refresh_tx: mpsc::Sender<()>,
+  pub stop_tx: mpsc::Sender<()>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProviderCache {
+  pub timestamp: Instant,
+  pub output: Box<ProviderOutput>,
+}
+
+/// Output emitted to frontend clients.
+#[derive(Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct ProviderOutput {
+  pub config_hash: String,
+  pub variables: VariablesResult,
+}
+
+/// Provider variable output emitted to frontend clients.
+///
+/// This is used instead of a normal `Result` type to serialize it in a
+/// nicer way.
+#[derive(Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub enum VariablesResult {
+  Data(ProviderVariables),
+  Error(String),
+}
+
+/// Implements conversion from an `anyhow::Result`.
+impl From<anyhow::Result<ProviderVariables>> for VariablesResult {
+  fn from(result: anyhow::Result<ProviderVariables>) -> Self {
+    match result {
+      Ok(data) => VariablesResult::Data(data),
+      Err(err) => VariablesResult::Error(err.to_string()),
+    }
+  }
+}
+
+impl ProviderRef {
+  pub fn new(
+    config_hash: String,
+    config: ProviderConfig,
+    emit_output_tx: mpsc::Sender<ProviderOutput>,
+    shared_state: &SharedProviderState,
+  ) -> anyhow::Result<Self> {
+    let provider = Self::create_provider(config, shared_state)?;
+
+    let (refresh_tx, refresh_rx) = mpsc::channel::<()>(1);
+    let (stop_tx, stop_rx) = mpsc::channel::<()>(1);
+
+    let min_refresh_interval = provider.min_refresh_interval();
+    let config_hash_clone = config_hash.clone();
+    let emit_output_tx_clone = emit_output_tx.clone();
+
+    task::spawn(async move {
+      Self::start_provider(
+        provider,
+        config_hash_clone,
+        emit_output_tx_clone,
+        refresh_rx,
+        stop_rx,
+      )
+    });
+
+    Ok(Self {
+      config_hash,
+      min_refresh_interval,
+      cache: None,
+      emit_output_tx,
+      refresh_tx,
+      stop_tx,
+    })
+  }
+
+  /// Starts the provider.
+  async fn start_provider(
+    mut provider: Box<dyn Provider + Send>,
+    config_hash: String,
+    emit_output_tx: mpsc::Sender<ProviderOutput>,
+    mut refresh_rx: mpsc::Receiver<()>,
+    mut stop_rx: mpsc::Receiver<()>,
+  ) {
+    info!("Starting provider: {}", config_hash);
+    provider
+      .on_start(&config_hash, emit_output_tx.clone())
+      .await;
+
+    // Loop to avoid exiting the select on refresh.
+    loop {
+      tokio::select! {
+        // On refresh, re-emit provider variables and continue looping.
+        Some(_) = refresh_rx.recv() => {
+          info!("Refreshing provider: {}", config_hash);
+          _ = provider.on_refresh(&config_hash, emit_output_tx.clone()).await;
+        },
+
+        // On stop, perform any necessary clean up and exit the loop.
+        Some(_) = stop_rx.recv() => {
+          info!("Stopping provider: {}", config_hash);
+          _ = provider.on_stop().await;
+          break;
+        },
+      }
+    }
+  }
+
+  fn create_provider(
+    config: ProviderConfig,
+    shared_state: &SharedProviderState,
+  ) -> anyhow::Result<Box<dyn Provider + Send>> {
+    let provider: Box<dyn Provider + Send> = match config {
+      ProviderConfig::Battery(config) => {
+        Box::new(BatteryProvider::new(config)?)
+      }
+      ProviderConfig::Cpu(config) => {
+        Box::new(CpuProvider::new(config, shared_state.sysinfo.clone()))
+      }
+      ProviderConfig::Host(config) => {
+        Box::new(HostProvider::new(config, shared_state.sysinfo.clone()))
+      }
+      ProviderConfig::Ip(config) => Box::new(IpProvider::new(config)),
+      #[cfg(windows)]
+      ProviderConfig::Komorebi(config) => {
+        Box::new(KomorebiProvider::new(config))
+      }
+      ProviderConfig::Memory(config) => {
+        Box::new(MemoryProvider::new(config, shared_state.sysinfo.clone()))
+      }
+      ProviderConfig::Network(config) => Box::new(NetworkProvider::new(
+        config,
+        shared_state.netinfo.clone(),
+      )),
+      ProviderConfig::Weather(config) => {
+        Box::new(WeatherProvider::new(config))
+      }
+      #[allow(unreachable_patterns)]
+      _ => bail!("Provider not supported on this operating system."),
+    };
+
+    Ok(provider)
+  }
+
+  /// Updates cache with the given output.
+  pub fn update_cache(&mut self, output: Box<ProviderOutput>) {
+    self.cache = Some(ProviderCache {
+      timestamp: Instant::now(),
+      output,
+    });
+  }
+
+  /// Refreshes the provider.
+  ///
+  /// Since the previous output of providers is cached, if within the
+  /// minimum refresh interval, send the previous output.
+  pub async fn refresh(&self) -> anyhow::Result<()> {
+    let min_refresh_interval =
+      self.min_refresh_interval.unwrap_or(Duration::MAX);
+
+    match &self.cache {
+      Some(cache) if cache.timestamp.elapsed() >= min_refresh_interval => {
+        self.emit_output_tx.send(*cache.output.clone()).await?;
+      }
+      _ => self.refresh_tx.send(()).await?,
+    };
+
+    Ok(())
+  }
+
+  /// Stops the given provider.
+  ///
+  /// This triggers any necessary cleanup.
+  pub async fn stop(&self) -> anyhow::Result<()> {
+    self.stop_tx.send(()).await?;
+
+    Ok(())
+  }
+}

packages/desktop/src/main.rs
@@ -1,6 +1,9 @@
 use std::{collections::HashMap, env, sync::Arc};
 
 use clap::Parser;
+use providers::{
+  config::ProviderConfig, provider_manager::init_provider_manager,
+};
 use serde::Serialize;
 use tauri::{
   AppHandle, Manager, State, WebviewUrl, WebviewWindowBuilder, Window,
@@ -18,7 +21,7 @@ use tracing_subscriber::EnvFilter;
 use crate::{
   cli::{Cli, CliCommand},
   monitors::get_monitors_str,
-  providers::{config::ProviderConfig, manager::ProviderManager},
+  providers::provider_manager::ProviderManager,
   sys_tray::setup_sys_tray,
   util::window_ext::WindowExt,
 };
@@ -72,7 +75,7 @@ async fn listen_provider(
   provider_manager: State<'_, ProviderManager>,
 ) -> anyhow::Result<(), String> {
   provider_manager
-    .listen(config_hash, config, tracked_access)
+    .create(config_hash, config, tracked_access)
     .await
     .map_err(|err| err.to_string())
 }
@@ -83,7 +86,7 @@ async fn unlisten_provider(
   provider_manager: State<'_, ProviderManager>,
 ) -> anyhow::Result<(), String> {
   provider_manager
-    .unlisten(config_hash)
+    .destroy(config_hash)
     .await
     .map_err(|err| err.to_string())
 }
@@ -169,7 +172,7 @@ async fn main() {
           // Add application icon to system tray.
           setup_sys_tray(app)?;
 
-          providers::manager::init(app)?;
+          init_provider_manager(app);
 
           let args_map = OpenWindowArgsMap(Default::default());
           let args_map_ref = args_map.0.clone();

packages/desktop/src/providers/battery/provider.rs
@@ -13,7 +13,7 @@ use tokio::task::AbortHandle;
 
 use super::{BatteryProviderConfig, BatteryVariables};
 use crate::providers::{
-  interval_provider::IntervalProvider, variables::ProviderVariables,
+  provider::IntervalProvider, variables::ProviderVariables,
 };
 
 pub struct BatteryProvider {

packages/desktop/src/providers/config.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-#[cfg(all(windows, target_arch = "x86_64"))]
+#[cfg(windows)]
 use super::komorebi::KomorebiProviderConfig;
 use super::{
   battery::BatteryProviderConfig, cpu::CpuProviderConfig,
@@ -16,7 +16,7 @@ pub enum ProviderConfig {
   Cpu(CpuProviderConfig),
   Host(HostProviderConfig),
   Ip(IpProviderConfig),
-  #[cfg(all(windows, target_arch = "x86_64"))]
+  #[cfg(windows)]
   Komorebi(KomorebiProviderConfig),
   Memory(MemoryProviderConfig),
   Network(NetworkProviderConfig),

packages/desktop/src/providers/cpu/provider.rs
@@ -6,7 +6,7 @@ use tokio::{sync::Mutex, task::AbortHandle};
 
 use super::{CpuProviderConfig, CpuVariables};
 use crate::providers::{
-  interval_provider::IntervalProvider, variables::ProviderVariables,
+  provider::IntervalProvider, variables::ProviderVariables,
 };
 
 pub struct CpuProvider {

packages/desktop/src/providers/host/provider.rs
@@ -6,7 +6,7 @@ use tokio::{sync::Mutex, task::AbortHandle};
 
 use super::{HostProviderConfig, HostVariables};
 use crate::providers::{
-  interval_provider::IntervalProvider, variables::ProviderVariables,
+  provider::IntervalProvider, variables::ProviderVariables,
 };
 
 pub struct HostProvider {

packages/desktop/src/providers/interval_provider.rs
@@ -1,124 +1,0 @@
-use std::{sync::Arc, time::Duration};
-
-use async_trait::async_trait;
-use tokio::{
-  sync::mpsc::Sender,
-  task::{self, AbortHandle},
-  time,
-};
-
-use super::{
-  manager::{ProviderOutput, VariablesResult},
-  provider::Provider,
-  variables::ProviderVariables,
-};
-
-/// Require interval providers to have a refresh interval in their config.
-pub trait IntervalConfig {
-  fn refresh_interval(&self) -> u64;
-}
-
-#[macro_export]
-macro_rules! impl_interval_config {
-  ($struct_name:ident) => {
-    use crate::providers::interval_provider::IntervalConfig;
-
-    impl IntervalConfig for $struct_name {
-      fn refresh_interval(&self) -> u64 {
-        self.refresh_interval
-      }
-    }
-  };
-}
-
-#[async_trait]
-pub trait IntervalProvider {
-  type Config: Sync + Send + 'static + IntervalConfig;
-  type State: Sync + Send + 'static;
-
-  /// Default to 2 seconds as the minimum refresh interval.
-  fn min_refresh_interval(&self) -> Duration {
-    Duration::from_secs(2)
-  }
-
-  fn config(&self) -> Arc<Self::Config>;
-
-  fn state(&self) -> Arc<Self::State>;
-
-  fn abort_handle(&self) -> &Option<AbortHandle>;
-
-  fn set_abort_handle(&mut self, abort_handle: AbortHandle);
-
-  async fn get_refreshed_variables(
-    config: &Self::Config,
-    state: &Self::State,
-  ) -> anyhow::Result<ProviderVariables>;
-}
-
-#[async_trait]
-impl<T: IntervalProvider + Send> Provider for T {
-  fn min_refresh_interval(&self) -> Duration {
-    T::min_refresh_interval(self)
-  }
-
-  async fn on_start(
-    &mut self,
-    config_hash: String,
-    emit_output_tx: Sender<ProviderOutput>,
-  ) {
-    let config = self.config();
-    let state = self.state();
-
-    let forever = task::spawn(async move {
-      let mut interval =
-        time::interval(Duration::from_millis(config.refresh_interval()));
-
-      loop {
-        // The first tick fires immediately.
-        interval.tick().await;
-
-        _ = emit_output_tx
-          .send(ProviderOutput {
-            config_hash: config_hash.clone(),
-            variables: to_variables_result(
-              T::get_refreshed_variables(&config, &state).await,
-            ),
-          })
-          .await;
-      }
-    });
-
-    self.set_abort_handle(forever.abort_handle());
-    _ = forever.await;
-  }
-
-  async fn on_refresh(
-    &mut self,
-    config_hash: String,
-    emit_output_tx: Sender<ProviderOutput>,
-  ) {
-    _ = emit_output_tx
-      .send(ProviderOutput {
-        config_hash,
-        variables: to_variables_result(
-          T::get_refreshed_variables(&self.config(), &self.state()).await,
-        ),
-      })
-      .await;
-  }
-
-  async fn on_stop(&mut self) {
-    if let Some(handle) = &self.abort_handle() {
-      handle.abort();
-    }
-  }
-}
-
-fn to_variables_result(
-  result: anyhow::Result<ProviderVariables>,
-) -> VariablesResult {
-  match result {
-    Ok(variables) => VariablesResult::Data(variables),
-    Err(err) => VariablesResult::Error(err.to_string()),
-  }
-}

packages/desktop/src/providers/ip/provider.rs
@@ -7,7 +7,7 @@ use tokio::task::AbortHandle;
 
 use super::{ipinfo_res::IpinfoRes, IpProviderConfig, IpVariables};
 use crate::providers::{
-  interval_provider::IntervalProvider, variables::ProviderVariables,
+  provider::IntervalProvider, variables::ProviderVariables,
 };
 
 pub struct IpProvider {

commit d89126c9d23284b6e85e4fc0ac5a47280cb7462b

fix: deadlock regression introduced by commit 8e585fa

packages/desktop/src/providers/provider_ref.rs
@@ -106,18 +106,26 @@ impl ProviderRef {
     mut refresh_rx: mpsc::Receiver<()>,
     mut stop_rx: mpsc::Receiver<()>,
   ) {
-    info!("Starting provider: {}", config_hash);
-    provider
-      .on_start(&config_hash, emit_output_tx.clone())
-      .await;
+    let mut has_started = false;
 
     // Loop to avoid exiting the select on refresh.
     loop {
+      let config_hash = config_hash.clone();
+      let emit_output_tx = emit_output_tx.clone();
+
       tokio::select! {
+        // Default match arm which handles initialization of the provider.
+        // This has a precondition to avoid running again on refresh.
+        _ = {
+          info!("Starting provider: {}", config_hash);
+          has_started = true;
+          provider.on_start(&config_hash, emit_output_tx.clone())
+        }, if !has_started => break,
+
         // On refresh, re-emit provider variables and continue looping.
         Some(_) = refresh_rx.recv() => {
           info!("Refreshing provider: {}", config_hash);
-          _ = provider.on_refresh(&config_hash, emit_output_tx.clone()).await;
+          _ = provider.on_refresh(&config_hash, emit_output_tx).await;
         },
 
         // On stop, perform any necessary clean up and exit the loop.