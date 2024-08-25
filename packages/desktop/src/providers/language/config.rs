9cb4d004f2d4c29610dc0af8108519ef8fbd88db
Andreas Olsson <olsson.andreas@gmail.com>
2024-08-25 13:13:25 +0200

    removed refresh_interval from language provider config

--- packages/desktop/src/providers/language/config.rs
+++ packages/desktop/src/providers/language/config.rs
@@ -1,11 +1,5 @@
 use serde::Deserialize;
 
-use crate::impl_interval_config;
-
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type", rename = "inputlanguage")]
-pub struct LanguageProviderConfig {
-  pub refresh_interval: u64,
-}
-
-impl_interval_config!(LanguageProviderConfig);
+pub struct LanguageProviderConfig {}
