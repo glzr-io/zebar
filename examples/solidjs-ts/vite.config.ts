4630463cd6731d94a0b56d40553761332f4b0352
Lars Berger <lars.berger@yahoo.com>
2024-09-01 04:32:56 +0800

    feat: add `examples` dir

---
+++ examples/solidjs-ts/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: { port: 3000 },
+  build: { target: 'esnext' },
+});
