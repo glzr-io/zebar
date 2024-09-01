53527da2531660d8ef1a7cc75b521e158050edbc
Lars Berger <lars.berger@yahoo.com>
2024-09-01 14:48:30 +0800

    feat: improve solid-ts example

---
+++ examples/solidjs-ts/src/App.tsx
@@ -0,0 +1,21 @@
+import styles from './App.module.css';
+
+export function App() {
+  return (
+    <div class={styles.app}>
+      <header class={styles.header}>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
