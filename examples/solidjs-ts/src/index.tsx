2b5d3012bd91b79aecfc673c45c2cb2d8d31af10
Lars Berger <lars.berger@yahoo.com>
2024-09-05 15:05:08 +0800

    feat: remove `glazewm` from solidjs-ts example

--- examples/solidjs-ts/src/index.tsx
+++ examples/solidjs-ts/src/index.tsx
@@ -5,7 +5,6 @@ import { createStore } from 'solid-js/store';
 import { init } from 'zebar';
 
 const zebarCtx = await init();
-const glazewm = await zebarCtx.createProvider({ type: 'glazewm' });
 const cpu = await zebarCtx.createProvider({ type: 'cpu' });
 const battery = await zebarCtx.createProvider({ type: 'battery' });
 const memory = await zebarCtx.createProvider({ type: 'memory' });
@@ -15,14 +14,12 @@ render(() => <App />, document.getElementById('root')!);
 
 function App() {
   const [store, setStore] = createStore({
-    glazewm: glazewm.output,
     cpu: cpu.output,
     battery: battery.output,
     memory: memory.output,
     weather: weather.output,
   });
 
-  glazewm.onOutput(glazewm => setStore({ glazewm }));
   cpu.onOutput(cpu => setStore({ cpu }));
   battery.onOutput(battery => setStore({ battery }));
   memory.onOutput(memory => setStore({ memory }));
@@ -30,7 +27,6 @@ function App() {
 
   return (
     <div>
-      glazewm: {JSON.stringify(store.glazewm)}
       cpu: {store.cpu.usage}
       battery: {store.battery?.chargePercent}
       memory: {store.memory.usage}
