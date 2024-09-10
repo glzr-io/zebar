4a189093c77f36e458fb2d5b286de4644ac6abe9
Lars Berger <lars.berger@yahoo.com>
2024-09-11 02:29:51 +0800

    feat: implement date provider formatting

--- packages/client-api/src/providers/date/create-date-provider.ts
+++ packages/client-api/src/providers/date/create-date-provider.ts
@@ -16,23 +16,24 @@ export interface DateProviderConfig {
 
   /**
    * Either a UTC offset (eg. `UTC+8`) or an IANA timezone (eg.
-   * `America/New_York`). Affects the output of `toFormat()`.
+   * `America/New_York`). Affects the output of {@link DateOutput.formatted}.
    *
    * A full list of available IANA timezones can be found [here](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).
    */
   timezone?: string;
 
   /**
-   * An ISO-639-1 locale, which is either a 2-letter language code (eg. `en`) or
-   * 4-letter language + country code (eg. `en-gb`). Affects the output of
-   * `toFormat()`.
+   * An ISO-639-1 locale, which is either a 2-letter language code
+   * (eg. `en`) or a 4-letter language + country code (eg. `en-gb`).
+   * Affects the output of {@link DateOutput.formatted}.
    *
    * A full list of ISO-639-1 locales can be found [here](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes#Table).
    */
   locale?: string;
 
   /**
-   * Formatting of the current date into a custom string format.
+   * Formatting of the current date into a custom string format. Affects
+   * the output of {@link DateOutput.formatted}.
    *
    * Refer to [table of tokens](https://moment.github.io/luxon/#/formatting?id=table-of-tokens)
    * for available date/time tokens.
@@ -47,13 +48,19 @@ export interface DateProviderConfig {
 const dateProviderConfigSchema = z.object({
   type: z.literal('date'),
   refreshInterval: z.coerce.number().default(1000),
-  timezone: z.string().optional(),
+  timezone: z.string().default('local'),
   locale: z.string().optional(),
+  formatting: z.string().default('EEE	d MMM t'),
 });
 
 export type DateProvider = Provider<DateProviderConfig, DateOutput>;
 
 export interface DateOutput {
+  /**
+   * Current date/time as a formatted string.
+   */
+  formatted: string;
+
   /**
    * Current date/time as a JavaScript `Date` object. Uses `new Date()` under
    * the hood.
@@ -73,8 +80,6 @@ export interface DateOutput {
   iso: string;
 }
 
-// TODO: Organize provider-related types.
-// TODO: Remove `toFormat` on date provider. Instead add `formatting` to the config.
 export async function createDateProvider(
   config: DateProviderConfig,
 ): Promise<DateProvider> {
@@ -89,13 +94,15 @@ export async function createDateProvider(
     );
 
     function getDateValue() {
-      const date = new Date();
+      const dateTime = DateTime.now().setZone(mergedConfig.timezone);
 
       return {
-        new: date,
-        now: date.getTime(),
-        iso: date.toISOString(),
-        formatted: 'todo',
+        new: dateTime.toJSDate(),
+        now: dateTime.toMillis(),
+        iso: dateTime.toISO()!,
+        formatted: dateTime.toFormat(mergedConfig.formatting, {
+          locale: mergedConfig.locale,
+        }),
       };
     }
 
