835f921312663cee7064473ea628f3a544cf8bf5
Lars Berger <lars.berger@yahoo.com>
2024-10-08 20:39:19 +0800

    chore: add smorf

--- packages/settings-ui/vite.config.ts
+++ packages/settings-ui/vite.config.ts
@@ -1,8 +1,17 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  server: {
+    port: 4200,
+  },
   build: { target: 'esnext' },
   base: './',
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src/app'),
+    },
+  },
 });
