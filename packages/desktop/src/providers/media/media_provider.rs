b7d84e56482711561a1a2579a2c22c92748de401
holby <justin.holby@aboutgolf.com>
2024-10-15 20:09:35 -0400

    WIP media provider attempt

--- packages/desktop/src/providers/media/media_provider.rs
+++ packages/desktop/src/providers/media/media_provider.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use tokio::{sync::mpsc::Sender, time};
 use windows::{
@@ -34,46 +35,89 @@ pub struct MediaOutput {
 }
 
 pub struct MediaProvider {
-  session_manager: GlobalSystemMediaTransportControlsSessionManager,
-  current_session: GlobalSystemMediaTransportControlsSession,
+  _config: MediaProviderConfig,
+  session_manager:
+    Option<GlobalSystemMediaTransportControlsSessionManager>,
+  current_session: Option<GlobalSystemMediaTransportControlsSession>,
 }
 
 impl MediaProvider {
-  pub fn new(
-    config: MediaProviderConfig,
-  ) -> anyhow::Result<MediaProvider> {
-    let session_manager =
-      GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?
-        .get()?;
-    let current_session = session_manager.GetCurrentSession()?;
-
-    let provider = MediaProvider {
-      session_manager,
-      current_session,
-    };
-
-    let media_props = provider.get_media_properties()?;
-    println!("{:?}", media_props.title);
-
-    Ok(provider)
+  pub fn new(config: MediaProviderConfig) -> Result<MediaProvider> {
+    Ok(MediaProvider {
+      _config: config,
+      session_manager: None,
+      current_session: None,
+    })
   }
 
-  fn get_media_properties(&self) -> Result<MediaOutput> {
-    let media_properties =
-      self.current_session.TryGetMediaPropertiesAsync()?.get()?;
-    Ok(MediaOutput {
-      title: media_properties.Title()?.to_string(),
-      artist: media_properties.Artist()?.to_string(),
-      album: media_properties.AlbumTitle()?.to_string(),
-      album_artist: media_properties.AlbumArtist()?.to_string(),
-    })
+  fn bind_media_events(&mut self) -> Result<()> {
+    if let Ok(session_manager) =
+      GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?
+        .get()
+    {
+      self.session_manager = Some(session_manager);
+      self.current_session = self
+        .session_manager
+        .as_ref()
+        .unwrap()
+        .GetCurrentSession()
+        .ok();
+
+      if let Some(current_session) = &self.current_session {
+        let media_properties_changed_handler = TypedEventHandler::new(
+          move |session: &Option<
+            GlobalSystemMediaTransportControlsSession,
+          >,
+                _| {
+            println!("Media properties changed event triggered.");
+            if let Some(session) = session {
+              if let Err(e) = Self::print_current_media_info(session) {
+                eprintln!("Failed to get media properties: {:?}", e);
+              }
+            } else {
+              println!("No session available on media properties change.");
+            }
+            Ok(())
+          },
+        );
+
+        if let Err(e) = current_session
+          .MediaPropertiesChanged(&media_properties_changed_handler)
+        {
+          eprintln!(
+            "Failed to attach media properties changed handler: {:?}",
+            e
+          );
+        } else {
+          println!("Media properties changed handler attached.");
+        }
+      }
+
+      Ok(())
+    } else {
+      Err(anyhow::anyhow!(
+        "Failed to request media transport controls session manager"
+      ))
+    }
+  }
+
+  fn print_current_media_info(
+    session: &GlobalSystemMediaTransportControlsSession,
+  ) -> Result<()> {
+    let media_properties = session.TryGetMediaPropertiesAsync()?.get()?;
+    println!("Title: {}", media_properties.Title()?);
+    println!("Artist: {}", media_properties.Artist()?);
+    println!("Album: {}", media_properties.AlbumTitle()?);
+    println!("Album Artist: {}", media_properties.AlbumArtist()?);
+    Ok(())
   }
 }
 
+#[async_trait]
 impl Provider for MediaProvider {
   async fn run(&self, emit_result_tx: Sender<ProviderResult>) {
-    if let Err(err) = self.create_socket(emit_result_tx.clone()).await {
-      emit_result_tx.send(Err(err).into()).await;
+    if let Err(err) = self.bind_media_events() {
+      // emit_result_tx.send(Err(err).into()).await;
     }
   }
 }
