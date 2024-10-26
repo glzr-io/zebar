e1667a6ba308d484c29a45c719925c5831c7cf0b
Lars Berger <lars.berger@yahoo.com>
2024-10-26 18:46:53 +0800

    style: run prettier

--- packages/settings-ui/tailwind.config.cjs
+++ packages/settings-ui/tailwind.config.cjs
@@ -1,7 +1,10 @@
-import tailwindConfig from '@glzr/components/tailwind.config.cjs'
+import tailwindConfig from '@glzr/components/tailwind.config.cjs';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...tailwindConfig,
-  content: ["src/**/*.{ts,tsx}", "./node_modules/@glzr/components/dist/**/*.js"],
+  content: [
+    'src/**/*.{ts,tsx}',
+    './node_modules/@glzr/components/dist/**/*.js',
+  ],
 };
