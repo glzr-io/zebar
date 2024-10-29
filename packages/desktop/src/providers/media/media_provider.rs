30ec6b6ec4a56b0fc05dcb625ab970b83df9d4a9
holby <justin.holby@aboutgolf.com>
2024-10-29 11:15:28 -0400

    fixed emit but broke everything else

--- packages/desktop/src/providers/media/media_provider.rs
+++ packages/desktop/src/providers/media/media_provider.rs
@@ -54,6 +54,7 @@ pub struct MediaProvider {
   _config: MediaProviderConfig,
   current_session: Arc<Mutex<Option<MediaSession>>>,
   event_tokens: Arc<Mutex<Option<EventTokens>>>,
+  media_output: Arc<Mutex<Option<MediaOutput>>>,
 }
 
 impl MediaProvider {
@@ -62,22 +63,28 @@ impl MediaProvider {
       _config: config,
       current_session: Arc::new(Mutex::new(None)),
       event_tokens: Arc::new(Mutex::new(None)),
+      media_output: Arc::new(Mutex::new(None)),
     }
   }
 
-  fn print_current_media_info(
+  fn emit_media_info(
     session: &MediaSession,
     emit_result_tx: Sender<ProviderResult>,
+    media_output: &mut MediaOutput,
   ) {
-    if let Ok(media_output) = Self::media_output(session) {
+    println!("Emitting media info.");
+    if let Ok(new_media_output) = Self::media_output(session) {
       info!("Title: {}", media_output.title);
       info!("Artist: {}", media_output.artist);
       info!("Album: {}", media_output.album);
       info!("Album Artist: {}", media_output.album_artist);
-      emit_result_tx.send(Ok(ProviderOutput::Media(media_output)).into());
+      if new_media_output != *media_output {
+        *media_output = new_media_output;
+        emit_result_tx
+          .try_send(Ok(ProviderOutput::Media(media_output.clone())).into())
+          .expect("Media output provider failed to send.");
+      }
     }
-
-    // TODO: Emit to frontend client via channel.
   }
 
   fn media_output(session: &MediaSession) -> anyhow::Result<MediaOutput> {
@@ -96,58 +103,43 @@ impl MediaProvider {
     emit_result_tx: Sender<ProviderResult>,
   ) -> anyhow::Result<()> {
     let session_manager = MediaManager::RequestAsync()?.get()?;
-    println!("Session manager obtained.");
     *self.current_session.lock().unwrap() =
       session_manager.GetCurrentSession().ok();
 
-    // TODO - better way of handling error?
-    match Self::add_session_listeners(
+    let tokens = Self::add_session_listeners(
       &self.current_session.lock().unwrap().as_ref().unwrap(),
       emit_result_tx.clone(),
-    ) {
-      Ok(tokens) => {
-        *self.event_tokens.lock().unwrap() = Some(tokens);
-      }
-      Err(err) => {
-        eprintln!("Error adding media session listeners: {:?}", err);
-      }
-    }
+      self.media_output.clone(),
+    );
+    *self.event_tokens.lock().unwrap() = tokens.ok();
 
     let current_session = self.current_session.clone();
     let event_tokens = self.event_tokens.clone();
+    let media_output = self.media_output.clone();
     let session_changed_handler = TypedEventHandler::new(
       move |session_manager: &Option<MediaManager>, _| {
         {
-          // Remove listeners from the previous session.
-          // Remove listeners from the previous session.
+          println!("Current session changed event triggered.");
           let mut current_session = current_session.lock().unwrap();
           let mut event_tokens = event_tokens.lock().unwrap();
-          if let Err(err) = Self::remove_session_listeners(
+          // let mut media_output = media_output.lock().unwrap();
+
+          // Remove listeners from the previous session.
+          Self::remove_session_listeners(
             &current_session.as_ref().unwrap(),
             event_tokens.as_ref(),
-          ) {
-            eprintln!("Error removing media session listeners: {:?}", err);
-          }
+          )
+          .expect("Error removing session listeners.");
 
           let new_session =
             MediaManager::RequestAsync()?.get()?.GetCurrentSession()?;
 
-          match Self::add_session_listeners(
-            &new_session,
+          let tokens = Self::add_session_listeners(
+            &current_session.as_ref().unwrap(),
             emit_result_tx.clone(),
-          ) {
-            Ok(tokens) => {
-              *event_tokens = Some(tokens);
-            }
-            Err(err) => {
-              eprintln!("Error adding media session listeners: {:?}", err);
-            }
-          }
-
-          Self::print_current_media_info(
-            &new_session,
-            emit_result_tx.clone(),
+            media_output.clone(),
           );
+          *event_tokens = tokens.ok();
           *current_session = Some(new_session);
         }
 
@@ -183,38 +175,57 @@ impl MediaProvider {
   fn add_session_listeners(
     session: &MediaSession,
     emit_result_tx: Sender<ProviderResult>,
+    media_output: Arc<Mutex<Option<MediaOutput>>>,
   ) -> anyhow::Result<EventTokens> {
     let media_properties_changed_handler = {
       let emit_result_tx = emit_result_tx.clone();
+      let media_output = media_output.clone();
       TypedEventHandler::new(move |session: &Option<MediaSession>, _| {
         println!("Media properties changed event triggered.");
         let session = session
           .as_ref()
           .expect("No session available on media properties change.");
-        Self::print_current_media_info(session, emit_result_tx.clone());
+        let mut media_output = media_output.lock().unwrap();
+        Self::emit_media_info(
+          session,
+          emit_result_tx.clone(),
+          media_output.as_mut().unwrap(),
+        );
         windows::core::Result::Ok(())
       })
     };
 
     let playback_info_changed_handler = {
       let emit_result_tx = emit_result_tx.clone();
+      let media_output = media_output.clone();
       TypedEventHandler::new(move |session: &Option<MediaSession>, _| {
         println!("Playback info changed event triggered.");
         let session = session
           .as_ref()
           .expect("No session available on playback info change.");
-        Self::print_current_media_info(session, emit_result_tx.clone());
+        let mut media_output = media_output.lock().unwrap();
+        Self::emit_media_info(
+          session,
+          emit_result_tx.clone(),
+          &mut media_output.as_mut().unwrap(),
+        );
         windows::core::Result::Ok(())
       })
     };
     let timeline_properties_changed_handler = {
       let emit_result_tx = emit_result_tx.clone();
+      let media_output = media_output.clone();
       TypedEventHandler::new(move |session: &Option<MediaSession>, _| {
         println!("Timeline properties changed event triggered.");
         let session = session
           .as_ref()
           .expect("No session available on timeline properties change.");
-        Self::print_current_media_info(session, emit_result_tx.clone());
+        let mut media_output = media_output.lock().unwrap();
+        Self::emit_media_info(
+          session,
+          emit_result_tx.clone(),
+          &mut media_output.as_mut().unwrap(),
+        );
         windows::core::Result::Ok(())
       })
     };
@@ -239,10 +250,11 @@ impl MediaProvider {
 #[async_trait]
 impl Provider for MediaProvider {
   async fn run(&self, emit_result_tx: Sender<ProviderResult>) {
-    if let Err(err) =
-      self.create_session_manager(emit_result_tx.clone())
-    {
-      emit_result_tx.send(Err(err).into());
+    if let Err(err) = self.create_session_manager(emit_result_tx.clone()) {
+      emit_result_tx
+        .send(Err(err).into())
+        .await
+        .expect("Error emitting media provider to frontend.");
     }
   }
 }
