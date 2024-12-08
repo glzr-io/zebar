ad72515c8f80c13fe80b048a9a725fb62be090fd
lars-berger <lars.berger@yahoo.com>
2024-12-08 19:19:37 +0800

    handle opaque responses in sw

--- packages/desktop/resources/sw.js
+++ packages/desktop/resources/sw.js
@@ -1,22 +1,39 @@
-console.log('Service worker loaded2');
-self.addEventListener('install', event => {
+console.log('Service worker loaded', Math.random());
+caches.open('v1').then(cache => {
+  cache.keys().then(keys => {
+    console.log('keys', keys);
+  });
+});
+self.addEventListener('install', () => {
+  // Skip waiting for activation. Only has an effect if there's a newly
+  // installed service worker that would otherwise remain in the `waiting`
+  // state.
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', () => {
+  // Claim clients to ensure that updates to the underlying service worker
+  // take effect immediately. Normally when a service worker is updated,
+  // pages won't use it until the next load.
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  console.log('fetch', event.request);
-  // Only cache GET requests.
-  if (event.request.method !== 'GET') {
+  // Use the default browser handling for requests where:
+  // - The request method is not GET.
+  // - The request is a navigation request.
+  // - The request is to the same origin as the service worker.
+  if (
+    event.request.method !== 'GET' ||
+    event.request.mode === 'navigate' ||
+    new URL(event.request.url).origin === self.location.origin
+  ) {
     return;
   }
 
   event.respondWith(
     (async () => {
-      // First try to get the resource from the cache.
+      // First, try to get the resource from the cache.
       const cache = await caches.open('v1');
       const cachedResponse = await cache.match(event.request);
 
@@ -28,10 +45,12 @@ self.addEventListener('fetch', event => {
         // Otherwise, fetch the resource from the network.
         const networkResponse = await fetch(event.request);
 
+        // Cache the response if its status is in the 200-299 range or if
+        // it's opaque. Opaque responses are from requests with 'no-cors',
+        // and have a status of 0.
         if (
           networkResponse &&
-          networkResponse.status === 200 &&
-          networkResponse.type !== 'opaque'
+          (networkResponse.ok || networkResponse.type === 'opaque')
         ) {
           await cache.put(event.request, networkResponse.clone());
         }
@@ -49,27 +68,16 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('message', event => {
-  const { type, key } = event.data;
+  const { type } = event.data;
 
-  if (type === 'CLEAR_CACHE' && typeof key === 'string') {
-    clearCacheByKey(key);
+  if (type === 'CLEAR_CACHE') {
+    event.waitUntil(
+      caches.open('v1').then(cache => cache.delete(event.data)),
+    );
   } else {
-    console.warn(
-      'Received unknown message type or invalid key:',
+    console.error(
+      'Service worker received unknown message type:',
       event.data,
     );
   }
 });
-
-const clearCacheByKey = async key => {
-  try {
-    const success = await caches.delete(key);
-    if (success) {
-      console.log(`Cache '${key}' deleted successfully.`);
-    } else {
-      console.log(`Cache '${key}' not found or could not be deleted.`);
-    }
-  } catch (error) {
-    console.error(`Error deleting cache '${key}':`, error);
-  }
-};
