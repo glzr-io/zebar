345addbbed7e025ec2f92f06915aba8b9edd5c12
lars-berger <lars.berger@yahoo.com>
2025-01-07 19:52:29 +0800

    feat: change `Nav` to be individual `SidebarItem` components

---
+++ packages/settings-ui/src/common/SidebarItem.tsx
@@ -0,0 +1,87 @@
+import {
+  buttonVariants,
+  cn,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@glzr/components';
+import { type JSX, Show } from 'solid-js';
+
+export interface SidebarItemProps {
+  isCollapsed: boolean;
+  title: string;
+  label?: string;
+  icon: JSX.Element;
+  variant: 'default' | 'ghost';
+}
+
+export function SidebarItem(props: SidebarItemProps) {
+  return (
+    <div
+      data-collapsed={props.isCollapsed}
+      class="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
+    >
+      <nav class="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        <Show
+          when={props.isCollapsed}
+          fallback={
+            <a
+              href="#"
+              class={cn(
+                buttonVariants({
+                  variant: props.variant,
+                  size: 'sm',
+                  class: 'text-sm',
+                }),
+                props.variant === 'default' &&
+                  'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
+                'justify-start',
+              )}
+            >
+              <div class="mr-2">{props.icon}</div>
+              {props.title}
+              {props.label && (
+                <span
+                  class={cn(
+                    'ml-auto',
+                    props.variant === 'default' &&
+                      'text-background dark:text-white',
+                  )}
+                >
+                  {props.label}
+                </span>
+              )}
+            </a>
+          }
+        >
+          <Tooltip openDelay={0} closeDelay={0} placement="right">
+            <TooltipTrigger
+              as="a"
+              href="#"
+              class={cn(
+                buttonVariants({
+                  variant: props.variant,
+                  size: 'icon',
+                }),
+                'h-9 w-9',
+                props.variant === 'default' &&
+                  'dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white',
+              )}
+            >
+              {props.icon}
+              <span class="sr-only">{props.title}</span>
+            </TooltipTrigger>
+            <TooltipContent class="flex items-center gap-4">
+              {props.title}
+              <Show when={props.label}>
+                <span class="ml-auto text-muted-foreground">
+                  {props.label}
+                </span>
+              </Show>
+            </TooltipContent>
+          </Tooltip>
+        </Show>
+      </nav>
+    </div>
+  );
+}
