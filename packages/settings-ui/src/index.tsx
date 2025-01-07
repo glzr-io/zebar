4a20b3d35f796ec342985cfd1145d6d5db9016e5
lars-berger <lars.berger@yahoo.com>
2025-01-07 16:48:25 +0800

    feat: add wip improved sidebar

--- packages/settings-ui/src/index.tsx
+++ packages/settings-ui/src/index.tsx
@@ -4,11 +4,12 @@ import { HashRouter, Route } from '@solidjs/router';
 import { render } from 'solid-js/web';
 
 import { WidgetConfigs } from './configs/WidgetConfigs';
+import Sidebar from './configs/Sidebar';
 
 render(
   () => (
     <HashRouter>
-      <Route path="/" component={WidgetConfigs} />
+      <Route path="/" component={Sidebar} />
       <Route path="/widget/:path" component={WidgetConfigs} />
     </HashRouter>
   ),
