6d6bb22a18f8c9da77a3ebf713df22700cb1187e
Lars Berger <lars.berger@yahoo.com>
2026-03-15 16:02:03 +0800

    fix: `top_most` z-order not working on macOS

--- packages/desktop/src/common/macos/window_ext_macos.rs
+++ packages/desktop/src/common/macos/window_ext_macos.rs
@@ -5,21 +5,28 @@ use cocoa::{
 };
 use tauri::{Runtime, Window};
 
+pub enum CustomWindowLevel {
+  AboveMenuBar,
+  // TODO: Use this for bottom-most windows.
+  Backstop,
+}
+
 pub trait WindowExtMacOs {
-  fn set_above_menu_bar(&self) -> anyhow::Result<()>;
+  fn set_level(&self, level: CustomWindowLevel) -> anyhow::Result<()>;
 }
 
 impl<R: Runtime> WindowExtMacOs for Window<R> {
-  fn set_above_menu_bar(&self) -> anyhow::Result<()> {
+  fn set_level(&self, level: CustomWindowLevel) -> anyhow::Result<()> {
     let ns_win =
       self.ns_window().context("Failed to get window handle.")? as id;
 
+    let level = match level {
+      CustomWindowLevel::AboveMenuBar => NSMainMenuWindowLevel as i64 + 1,
+      CustomWindowLevel::Backstop => -20,
+    };
+
     unsafe {
-      ns_win.setLevel_(
-        ((NSMainMenuWindowLevel + 1) as u64)
-          .try_into()
-          .context("Failed to cast `NSMainMenuWindowLevel`.")?,
-      );
+      ns_win.setLevel_(level);
     }
 
     Ok(())
